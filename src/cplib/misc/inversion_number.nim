0c6ac8a4a4d438716ac15c8d1e0f71706ba0ace9
seekworser <seekworser1963@gmail.com>
2023-11-02 03:05:27 +0900

    fix

--- src/cplib/misc/inversion_number.nim
+++ src/cplib/misc/inversion_number.nim
@@ -1,18 +1,14 @@
-
 when not declared CPLIB_MISC_INVERSION_NUMBER:
     const COMPETITIVE_MISC_INVERSION_NUMBER* = 1
     import algorithm, sequtils
-    import atcoder/segtree
-    proc op(x, y: int): int = return x+y
-    proc e(): int = return 0
+    import atcoder/fenwicktree
     proc inversion_number*(a: seq[int]): int =
         let c = a.sorted.deduplicate(isSorted = true)
-        #FIXME: use BIT
-        var seg = initSegTree[int](c.len, op, e)
+        var tree = initFenwickTree[int](c.len)
         var ans = 0
         for i in 0..<a.len:
             var pos = c.lowerBound(a[i])
             if pos + 1 < c.len:
-                ans += seg.prod(pos+1..<c.len)
-            seg.set(pos, seg.get(pos)+1)
+                ans += tree.sum(pos+1..<c.len)
+            tree.add(pos, 1)
         return ans
