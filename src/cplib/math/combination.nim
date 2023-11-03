16130030fdd055e185aeb8add19768c4fcf3602a
seekworser <seekworser1963@gmail.com>
2023-11-04 04:24:40 +0900

    change template params to ModInt

--- src/cplib/math/combination.nim
+++ src/cplib/math/combination.nim
@@ -1,35 +1,36 @@
 when not declared CPLIB_MATH_COMBINATION:
     const COMPETITIVE_MATH_COMBINATION* = 1
-    type Combination_Type[T]= object
-        fact : seq[T]
-        inv : seq[T]
-        fact_inv : seq[T]
-    
-    proc initCombination*[T](max_N:int):Combination_Type[T]=
-        result.fact = newSeq[T](max_N+1)
-        result.inv = newSeq[T](max_N+1)
-        result.fact_inv = newSeq[T](max_N+1)
+    import atcoder/modint
+    type Combination_Type[ModInt] = object
+        fact: seq[ModInt]
+        inv: seq[ModInt]
+        fact_inv: seq[ModInt]
+
+    proc initCombination*[ModInt](max_N: int): Combination_Type[ModInt] =
+        result.fact = newSeq[ModInt](max_N+1)
+        result.inv = newSeq[ModInt](max_N+1)
+        result.fact_inv = newSeq[ModInt](max_N+1)
         result.fact[0] = 1
-        result.fact[1] = 1  
+        result.fact[1] = 1
         result.inv[1] = 1
         result.fact_inv[0] = 1
         result.fact_inv[1] = 1
         for i in 2..max_N:
             result.fact[i] = result.fact[i-1]*i
-            result.inv[i] = -result.inv[int(T.umod()) mod i]*(int(T.umod()) div i)
+            result.inv[i] = -result.inv[int(ModInt.umod()) mod i]*(int(ModInt.umod()) div i)
             result.fact_inv[i] = result.fact_inv[i-1]*result.inv[i]
-    
-    proc ncr*[T](c:Combination_Type[T],n,r:int):T=
+
+    proc ncr*[ModInt](c: Combination_Type[ModInt], n, r: int): ModInt =
         if n < 0 or r < 0:
             return 0
         if n < r:
             return 0
         return c.fact[n]*c.fact_inv[n-r]*c.fact_inv[r]
-    proc npr*[T](c:Combination_Type[T],n,r:int):T=
+    proc npr*[ModInt](c: Combination_Type[ModInt], n, r: int): ModInt =
         if n < 0 or r < 0:
             return 0
         if n < r:
             return 0
         return c.fact[n]*c.fact_inv[r]
-    proc nhr*[T](c:Combination_Type[T],n,r:int):T=
-        return c.ncr(n+r-1,r)
+    proc nhr*[ModInt](c: Combination_Type[ModInt], n, r: int): ModInt =
+        return c.ncr(n+r-1, r)
---
+++ src/verify/math/combination_test.nim
@@ -0,0 +1,3 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/binomial_coefficient_prime_mod
+import atcoder/modint
+import cplib/math/combination
