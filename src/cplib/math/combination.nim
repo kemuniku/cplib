72327738d88cce64d9a00ffc7ed478bc35a0fffb
seekworser <seekworser1963@gmail.com>
2023-11-04 05:28:46 +0900

    change to use var

--- src/cplib/math/combination.nim
+++ src/cplib/math/combination.nim
@@ -1,24 +1,25 @@
 when not declared CPLIB_MATH_COMBINATION:
     const COMPETITIVE_MATH_COMBINATION* = 1
     import atcoder/modint
-    type Combination_Type[ModInt] = object
+    type Combination_Type[ModInt] = ref object
         fact: seq[ModInt]
         inv: seq[ModInt]
         fact_inv: seq[ModInt]
 
     proc initCombination*[ModInt](max_N: int): Combination_Type[ModInt] =
-        result.fact = newSeq[ModInt](max_N+1)
-        result.inv = newSeq[ModInt](max_N+1)
-        result.fact_inv = newSeq[ModInt](max_N+1)
-        result.fact[0] = 1
-        result.fact[1] = 1
-        result.inv[1] = 1
-        result.fact_inv[0] = 1
-        result.fact_inv[1] = 1
+        var fact = newSeq[ModInt](max_N+1)
+        var inv = newSeq[ModInt](max_N+1)
+        var fact_inv = newSeq[ModInt](max_N+1)
+        fact[0] = 1
+        fact[1] = 1
+        inv[1] = 1
+        fact_inv[0] = 1
+        fact_inv[1] = 1
         for i in 2..max_N:
-            result.fact[i] = result.fact[i-1]*i
-            result.inv[i] = -result.inv[int(ModInt.umod()) mod i]*(int(ModInt.umod()) div i)
-            result.fact_inv[i] = result.fact_inv[i-1]*result.inv[i]
+            fact[i] = fact[i-1] * i
+            inv[i] = -inv[int(ModInt.umod()) mod i]*(int(ModInt.umod()) div i)
+            fact_inv[i] = fact_inv[i-1] * inv[i]
+        result = Combination_Type[ModInt](fact: fact, inv: inv, fact_inv: fact_inv)
 
     proc ncr*[ModInt](c: Combination_Type[ModInt], n, r: int): ModInt =
         if n < 0 or r < 0:
