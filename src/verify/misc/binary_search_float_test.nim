8c5a17ca074f7551d9dbd29821f0d1e739f0e720
seekworser <seekworser1963@gmail.com>
2023-11-04 02:22:03 +0900

    add binary search float test

---
+++ src/verify/misc/binary_search_float_test.nim
@@ -0,0 +1,9 @@
+# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/ITP1_4_A
+include cplib/tmpl/citrus
+import cplib/misc/binary_search
+
+var a, b = input(int)
+var div_int = meguru_bisect(0, a+1, proc(x: int): bool = a >= b * x)
+var rem = a - b * div_int
+var div_flaot = meguru_bisect(0.0, float64(a + 1), (x)=>(a >= b * x))
+print(div_int, rem, div_flaot)
