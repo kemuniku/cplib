c24aba1d876947d0e20f752c21bfef24da20be9e
kemuniku <kemuni9@gmail.com>
2023-11-07 02:27:06 +0900

    add restore proc

--- src/cplib/graph/dijkstra.nim
+++ src/cplib/graph/dijkstra.nim
@@ -1,8 +1,9 @@
 when not declared CPLIB_GRAPH_DIJKSTRA:
     import cplib/graph/graph
     import std/heapqueue
+    import algorithm
     const CPLIB_GRAPH_DIJKSTRA* = 1 
-    proc restore_dijkstra*[T](G:Graph[T],start:int,ZERO:T = 0,INF:T = int(3300300300300300491)):(seq[T],seq[int])=
+    proc restore_dijkstra*[T](G:Graph[T],start:int,ZERO:T = 0,INF:T = int(3300300300300300491)):tuple[costs:seq[T],prev:seq[int]]=
         var
             queue = initHeapQueue[(T,int)]()
             costs = newSeq[T](len(G.edges))
@@ -25,12 +26,13 @@ when not declared CPLIB_GRAPH_DIJKSTRA:
     proc dijkstra*[T](G:Graph[T],start:int,ZERO:T = 0,INF:T = int(3300300300300300491)):seq[T]=
         var costs,_ = restore_dijkstra(G,start,ZERO,INF)
         return costs
-    proc shortest_path*[T](G:Graph[T],start:int,goal:int,ZERO:T = 0,INF:T = int(3300300300300300491)):tuple[path:seq[T],cost:int]=
-        var (costs,prev) = restore_dijkstra(G,start,ZERO,INF)
-        var i:int
-        i = goal
+    proc restore_shortestpath_from_prev*(prev:seq[int],goal:int):seq[int]=
+        var i = goal
         while i != -1:
-            result.path.add(i)
+            result.add(i)
             i = prev[i]
-        result.path.reverse()
+        result = result.reversed()
+    proc shortest_path*[T](G:Graph[T],start:int,goal:int,ZERO:T = 0,INF:T = int(3300300300300300491)):tuple[path:seq[int],cost:int]=
+        var (costs,prev) = restore_dijkstra(G,start,ZERO,INF)
+        result.path = prev.restore_shortestpath_from_prev(goal)
         result.cost = costs[goal]
