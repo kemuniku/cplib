2806393ce7199e808f3886c049aad68a8e12a07e
kemuniku <kemuni9@gmail.com>
2023-11-06 22:48:25 +0900

    delete fenwick_tree.nim

--- src/cplib/data_structure/fenwick_tree.nim
+++
@@ -1,9 +0,0 @@
-when not declared CPLIB_DATA_STRUCTURE_FENWICK_TREE:
-    const CPLIB_DATA_STRUCTURE_FENWICK_TREE * = 1
-    import std/sequtils
-
-    type FenwickTree_Type*[T] = object
-        data*: seq[T]
-
-    proc initFenwickTree*[T](n: int): FenwickTree_Type[T] =
-        return FenwickTree_Type()
