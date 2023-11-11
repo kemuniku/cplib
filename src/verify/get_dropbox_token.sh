ad153a91ef5287b40e0d8002e0eda52dbb119660
seekworser <seekworser1963@gmail.com>
2023-11-11 17:35:49 +0900

    minor fix

--- src/verify/get_dropbox_token.sh
+++ src/verify/get_dropbox_token.sh
@@ -3,4 +3,4 @@ echo $(curl --silent https://api.dropbox.com/oauth2/token \
     --user ${DROPBOX_APP_KEY}:${DROPBOX_APP_SECRET} \
     --data grant_type=refresh_token \
     --data refresh_token=${DROPBOX_REFRESH_TOKEN}) \
-    | jq -r .access_token
+    | echo $(jq -r .access_token)
