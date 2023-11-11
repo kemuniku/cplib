3953b8ff6a034bb83cbaf556dc9dd6fa8873b163
seekworser <seekworser1963@gmail.com>
2023-11-12 02:51:46 +0900

    remove unused file

--- src/verify/get_dropbox_token.sh
+++
@@ -1,7 +0,0 @@
-RESPONSE=$( \
-    curl --silent https://api.dropbox.com/oauth2/token \
-    --user ${DROPBOX_APP_KEY}:${DROPBOX_APP_SECRET} \
-    --data grant_type=refresh_token \
-    --data refresh_token=${DROPBOX_REFRESH_TOKEN} \
-)
-export DROPBOX_TOKEN=$(echo $RESPONSE | jq -r .access_token)
