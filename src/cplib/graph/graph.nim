bb1f05dd4c7f69725e46e5d36203e05c7caa3d7c
seekworser <seekworser1963@gmail.com>
2023-11-16 00:40:08 +0900

    add tree tmpl (wip)

--- src/cplib/graph/graph.nim
+++ src/cplib/graph/graph.nim
@@ -1,36 +1,40 @@
 when not declared CPLIB_GRAPH_GRAPH:
-    const CPLIB_GRAPH_GRAPH* = 1 
+    const CPLIB_GRAPH_GRAPH* = 1
 
     type Graph*[T] = ref object of RootObj
-        edges*:seq[seq[(int,T)]]
-    
-    type WeightedDirectedGraph*[T] =  ref object of Graph[T]
-    type WeightedUnDirectedGraph*[T] =  ref object of Graph[T]
-    type UnWeightedDirectedGraph* =  ref object of Graph[int]
-    type UnWeightedUnDirectedGraph* =  ref object of Graph[int]
-    
+        edges*: seq[seq[(int, T)]]
+
+    type WeightedDirectedGraph*[T] = ref object of Graph[T]
+    type WeightedUnDirectedGraph*[T] = ref object of Graph[T]
+    type UnWeightedDirectedGraph* = ref object of Graph[int]
+    type UnWeightedUnDirectedGraph* = ref object of Graph[int]
+
+    type GraphTypes* = Graph or WeightedDirectedGraph or WeightedUnDirectedGraph or UnWeightedDirectedGraph or UnWeightedUnDirectedGraph
+
+    proc add_edge_impl[T](g: GraphTypes, u, v: int, cost: T, directed: bool) =
+        g.edges[u].add((v, cost))
+        if not directed: g.edges[v].add((u, cost))
+
     #WeightedDirectedGraph
-    proc initWeightedDirectedGraph*(N:int,edgetype:typedesc = int):WeightedDirectedGraph[edgetype]=
-        result = WeightedDirectedGraph[edgetype](edges:newSeq[seq[(int,edgetype)]](N))
-    proc add_edge*[T](g:var WeightedDirectedGraph[T],u,v:int,cost:T)=
-        g.edges[u].add((v,cost))
-    
+    proc initWeightedDirectedGraph*(N: int, edgetype: typedesc = int): WeightedDirectedGraph[edgetype] =
+        result = WeightedDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N))
+    proc add_edge*[T](g: var WeightedDirectedGraph[T], u, v: int, cost: T) =
+        g.add_edge_impl(u, v, cost, true)
+
     #WeightedUnDirectedGraph
-    proc initWeightedUnDirectedGraph*(N:int,edgetype:typedesc = int):WeightedUnDirectedGraph[edgetype]=
-        result = WeightedUnDirectedGraph[edgetype](edges:newSeq[seq[(int,edgetype)]](N))
-    proc add_edge*[T](g:var WeightedUnDirectedGraph[T],u,v:int,cost:T)=
-        g.edges[u].add((v,cost))
-        g.edges[v].add((u,cost))
-    
+    proc initWeightedUnDirectedGraph*(N: int, edgetype: typedesc = int): WeightedUnDirectedGraph[edgetype] =
+        result = WeightedUnDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N))
+    proc add_edge*[T](g: var WeightedUnDirectedGraph[T], u, v: int, cost: T) =
+        g.add_edge_impl(u, v, cost, false)
+
     #UnWeightedDirectedGraph
-    proc initUnWeightedDirectedGraph*(N:int):UnWeightedDirectedGraph=
-        result = UnWeightedDirectedGraph(edges:newSeq[seq[(int,int)]](N))
-    proc add_edge*[T](g:var UnWeightedDirectedGraph,u,v:int)=
-        g.edges[u].add((v,1))
-    
+    proc initUnWeightedDirectedGraph*(N: int): UnWeightedDirectedGraph =
+        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](N))
+    proc add_edge*(g: var UnWeightedDirectedGraph, u, v: int) =
+        g.add_edge_impl(u, v, 1, true)
+
     #UnWeightedUnDirectedGraph
-    proc initUnWeightedUnDirectedGraph*(N:int):UnWeightedUnDirectedGraph=
-        result = UnWeightedUnDirectedGraph(edges:newSeq[seq[(int,int)]](N))
-    proc add_edge*[T](g:var UnWeightedUnDirectedGraph,u,v:int)=
-        g.edges[u].add((v,1))
-        g.edges[v].add((u,1))
+    proc initUnWeightedUnDirectedGraph*(N: int): UnWeightedUnDirectedGraph =
+        result = UnWeightedUnDirectedGraph(edges: newSeq[seq[(int, int)]](N))
+    proc add_edge*(g: var UnWeightedUnDirectedGraph, u, v: int) =
+        g.add_edge_impl(u, v, 1, false)
---
+++ src/cplib/tree/prufer.nim
@@ -0,0 +1,2 @@
+when not declared CPLIB_TREE_PRUFER:
+    const CPLIB_TREE_PRUFER* = 1
