bcd681c8806d8adfc78ba47cdb2ac27ce727a2cf
GitHub Action <EMAIL>
2023-11-21 14:11:07 +0000

    [workflow] format 2892ef92f080879bee138bf993bbccc94f85dbdb

--- src/verify/collections/QSWAG_test.nim
+++ src/verify/collections/QSWAG_test.nim
@@ -3,19 +3,19 @@ import cplib/collections/QSWAG
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
 const MOD = 998244353
-proc op(a,b:(int,int)):(int,int)=
-    return ((a[0]*b[0]) mod MOD, ((a[1]*b[0] mod MOD) + b[1] )mod MOD)
+proc op(a, b: (int, int)): (int, int) =
+    return ((a[0]*b[0]) mod MOD, ((a[1]*b[0] mod MOD) + b[1]) mod MOD)
 
 var Q = ii()
-var swag = initSWAG(op,(1,0))
+var swag = initSWAG(op, (1, 0))
 for i in 0..<Q:
     var q = ii()
     if q == 0:
-        var a,b = ii()
-        swag.push((a,b))
+        var a, b = ii()
+        swag.push((a, b))
     elif q == 1:
         discard swag.pop()
     else:
         var x = ii()
-        var (a,b) = swag.fold()
+        var (a, b) = swag.fold()
         echo (a*x mod MOD + b) mod MOD
