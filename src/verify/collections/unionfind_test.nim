8e462d2ac1c57c1c628feebf08575cfb9097d987
kemuniku <kemuni9@gmail.com>
2023-11-21 13:10:39 +0900

    add_unionfindtest

---
+++ src/verify/collections/unionfind_test.nim
@@ -0,0 +1,18 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/unionfind
+import math,sequtils
+import cplib/collections/unionfind
+
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+
+var N,Q = ii()
+var uf = initUnionFind(N)
+for i in 0..<Q:
+    var t,u,v = ii()
+    if t == 0:
+        uf.unite(u,v)
+    else:
+        if uf.issame(u,v):
+            echo 1
+        else:
+            echo 0
