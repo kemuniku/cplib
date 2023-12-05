b29b32c6ca1e88171cdb1a04387611a120f57fc8
GitHub Action <EMAIL>
2023-12-05 14:32:07 +0000

    [workflow] format 67dc304b8cbe829080cc4b18e2f39f881c5a705d

--- src/verify/itertools/itertools_combinations_2_test.nim
+++ src/verify/itertools/itertools_combinations_2_test.nim
@@ -5,10 +5,10 @@ import cplib/itertools/combinations
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
 
-var N,K = ii()
-var A = newSeqWith(N,ii())
+var N, K = ii()
+var A = newSeqWith(N, ii())
 var ans = 0
-for x in combinations(A,K):
+for x in combinations(A, K):
     var S = sum(x)
     if S mod 998 >= S mod 998244353:
         ans += 1
