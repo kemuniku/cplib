a329eb5fc7b36475313c77a07506783808b86bc1
GitHub Action <EMAIL>
2023-12-11 17:37:24 +0000

    [workflow] format 86054c91170bbe844b1cccc044587c12fc8e1979

--- src/cplib/utils/bititers.nim
+++ src/cplib/utils/bititers.nim
@@ -1,7 +1,7 @@
 when not declared CPLIB_UTILS_BITITERS:
     const CPLIB_UTILS_BITITERS* = 1
     import bitops
-    iterator bitcomb*(n,r:int):int=
+    iterator bitcomb*(n, r: int): int =
         ##n bit中 r bitが1であるようなbit列を列挙します。
         var x = (1 shl r)-1
         while true:
@@ -10,24 +10,24 @@ when not declared CPLIB_UTILS_BITITERS:
             x = (t+1) or (((not t and - not t) - 1) shr (x.countTrailingZeroBits() + 1))
             if x >= (1 shl n):
                 break
-    
-    iterator bitsubseteq*(bits:int):int=
+
+    iterator bitsubseteq*(bits: int): int =
         ##与えられた集合の部分集合を昇順で列挙します。与えられた集合も含みます。
         var i = 0
         while true:
             yield i
             if bits == i:
-                break 
+                break
             i = (i-bits) and bits
-    iterator bitsubset*(bits:int):int=
+    iterator bitsubset*(bits: int): int =
         ##与えられた集合の部分集合を昇順で列挙します。与えられた集合は含みません。
         var i = 0
         while true:
             yield i
             i = (i-bits) and bits
             if bits == i:
-                break 
-    iterator bitsubset_descendingeq*(bits:int):int=
+                break
+    iterator bitsubset_descendingeq*(bits: int): int =
         ##与えられた集合の部分集合を昇順で列挙します。与えられた集合も含みます。
         var i = bits
         while true:
@@ -35,7 +35,7 @@ when not declared CPLIB_UTILS_BITITERS:
             if i == 0:
                 break
             i = (i-1) and bits
-    iterator bitsubset_descending*(bits:int):int=
+    iterator bitsubset_descending*(bits: int): int =
         ##与えられた集合の部分集合を昇順で列挙します。与えられた集合は含みません。
         var i = bits
         while true:
@@ -44,8 +44,8 @@ when not declared CPLIB_UTILS_BITITERS:
             if i == 0:
                 break
 
-    
-    iterator bitsuperset*(bits,n:int):int=
+
+    iterator bitsuperset*(bits, n: int): int =
         ## 与えられた集合を包含する集合を列挙します。bit数上限をnとします。
         var i = bits
         while true:
@@ -54,7 +54,7 @@ when not declared CPLIB_UTILS_BITITERS:
             if i >= (1 shl n):
                 break
 
-    iterator bitsingleton*(bits:int):int=
+    iterator bitsingleton*(bits: int): int =
         ##立っているbitを一つずつ取り出します。
         var i = bits and (-bits)
         while true:
@@ -63,7 +63,7 @@ when not declared CPLIB_UTILS_BITITERS:
             if i == 0:
                 break
 
-    iterator standingbits*(bits:int):int=
+    iterator standingbits*(bits: int): int =
         #bits & (1<<i)が0でない値になるようなiを列挙します。
         var i = bits and (-bits)
         while true:
