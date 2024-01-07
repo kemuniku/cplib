68fe1bce17b6641f7b0beb2d5e26d1ac71fd3f8d
GitHub Action <EMAIL>
2024-01-07 09:42:27 +0000

    [workflow] format 68f18657482d69ba56361a80abc6e5f547560f07

--- src/cplib/graph/graph.nim
+++ src/cplib/graph/graph.nim
@@ -3,7 +3,7 @@ when not declared CPLIB_GRAPH_GRAPH:
 
     type Graph*[T] = ref object of RootObj
         edges*: seq[seq[(int, T)]]
-        N*:int
+        N*: int
 
     type WeightedDirectedGraph*[T] = ref object of Graph[T]
     type WeightedUnDirectedGraph*[T] = ref object of Graph[T]
@@ -19,27 +19,27 @@ when not declared CPLIB_GRAPH_GRAPH:
 
     #WeightedDirectedGraph
     proc initWeightedDirectedGraph*(N: int, edgetype: typedesc = int): WeightedDirectedGraph[edgetype] =
-        result = WeightedDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N),N:N)
+        result = WeightedDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N), N: N)
     proc add_edge*[T](g: var WeightedDirectedGraph[T], u, v: int, cost: T) =
         g.add_edge_impl(u, v, cost, true)
 
     #WeightedUnDirectedGraph
     proc initWeightedUnDirectedGraph*(N: int, edgetype: typedesc = int): WeightedUnDirectedGraph[edgetype] =
-        result = WeightedUnDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N),N:N)
+        result = WeightedUnDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N), N: N)
     proc add_edge*[T](g: var WeightedUnDirectedGraph[T], u, v: int, cost: T) =
         g.add_edge_impl(u, v, cost, false)
 
     #UnWeightedDirectedGraph
     proc initUnWeightedDirectedGraph*(N: int): UnWeightedDirectedGraph =
-        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](N),N:N)
+        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](N), N: N)
     proc add_edge*(g: var UnWeightedDirectedGraph, u, v: int) =
         g.add_edge_impl(u, v, 1, true)
 
     #UnWeightedUnDirectedGraph
     proc initUnWeightedUnDirectedGraph*(N: int): UnWeightedUnDirectedGraph =
-        result = UnWeightedUnDirectedGraph(edges: newSeq[seq[(int, int)]](N),N:N)
+        result = UnWeightedUnDirectedGraph(edges: newSeq[seq[(int, int)]](N), N: N)
     proc add_edge*(g: var UnWeightedUnDirectedGraph, u, v: int) =
         g.add_edge_impl(u, v, 1, false)
-    
 
-    proc len*(G:GraphTypes):int=G.N
+
+    proc len*(G: GraphTypes): int = G.N
--- src/cplib/graph/topologicalsort.nim
+++ src/cplib/graph/topologicalsort.nim
@@ -1,18 +1,18 @@
 when not declared CPLIB_GRAPH_TOPOLOGICALSORT:
     import cplib/graph/graph
-    proc topologicalsort*(G:DirectedGraph):seq[int]=
+    proc topologicalsort*(G: DirectedGraph): seq[int] =
         var gin = newseq[int](G.N)
         for i in 0..<G.N:
-            for (j,_) in G.edges[i]:
+            for (j, _) in G.edges[i]:
                 gin[j] += 1
-        var stack : seq[int]
+        var stack: seq[int]
         for i in 0..<G.N:
             if gin[i] == 0:
                 stack.add(i)
         while len(stack) != 0:
             var i = stack.pop()
             result.add(i)
-            for (j,_) in G.edges[i]:
+            for (j, _) in G.edges[i]:
                 gin[j] -= 1
                 if gin[j] == 0:
                     stack.add(j)
