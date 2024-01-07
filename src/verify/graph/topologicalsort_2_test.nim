943a7c874d40ad5b1114a16b3440bc8955637d1a
kemuniku <kemuni9@gmail.com>
2024-01-07 20:02:14 +0900

    change url

--- src/verify/graph/topologicalsort_2_test.nim
+++ src/verify/graph/topologicalsort_2_test.nim
@@ -1,11 +1,11 @@
-# verification-helper: PROBLEM https://atcoder.jp/contests/abc216/tasks/abc216_d
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc245/tasks/abc245_f
 import cplib/graph/graph
 import cplib/graph/topologicalsort
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
-var N, M = ii()
+var N,M = ii()
 var G = initUnWeightedDirectedGraph(N)
 for i in 0..<M:
-    var U, V = ii()-1
-    G.add_edge(V, U)
+    var U,V = ii()-1
+    G.add_edge(V,U)
 echo N-len(G.topologicalsort())
