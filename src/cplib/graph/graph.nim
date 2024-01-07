68f18657482d69ba56361a80abc6e5f547560f07
kemuniku <kemuni9@gmail.com>
2024-01-07 18:33:12 +0900

    add N to Graph

--- src/cplib/graph/graph.nim
+++ src/cplib/graph/graph.nim
@@ -3,6 +3,7 @@ when not declared CPLIB_GRAPH_GRAPH:
 
     type Graph*[T] = ref object of RootObj
         edges*: seq[seq[(int, T)]]
+        N*:int
 
     type WeightedDirectedGraph*[T] = ref object of Graph[T]
     type WeightedUnDirectedGraph*[T] = ref object of Graph[T]
@@ -10,31 +11,35 @@ when not declared CPLIB_GRAPH_GRAPH:
     type UnWeightedUnDirectedGraph* = ref object of Graph[int]
 
     type GraphTypes* = Graph or WeightedDirectedGraph or WeightedUnDirectedGraph or UnWeightedDirectedGraph or UnWeightedUnDirectedGraph
-
+    type DirectedGraph* = WeightedDirectedGraph or UnWeightedDirectedGraph
+    type WeightedGraph* = WeightedDirectedGraph or WeightedUnDirectedGraph
     proc add_edge_impl[T](g: GraphTypes, u, v: int, cost: T, directed: bool) =
         g.edges[u].add((v, cost))
         if not directed: g.edges[v].add((u, cost))
 
     #WeightedDirectedGraph
     proc initWeightedDirectedGraph*(N: int, edgetype: typedesc = int): WeightedDirectedGraph[edgetype] =
-        result = WeightedDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N))
+        result = WeightedDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N),N:N)
     proc add_edge*[T](g: var WeightedDirectedGraph[T], u, v: int, cost: T) =
         g.add_edge_impl(u, v, cost, true)
 
     #WeightedUnDirectedGraph
     proc initWeightedUnDirectedGraph*(N: int, edgetype: typedesc = int): WeightedUnDirectedGraph[edgetype] =
-        result = WeightedUnDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N))
+        result = WeightedUnDirectedGraph[edgetype](edges: newSeq[seq[(int, edgetype)]](N),N:N)
     proc add_edge*[T](g: var WeightedUnDirectedGraph[T], u, v: int, cost: T) =
         g.add_edge_impl(u, v, cost, false)
 
     #UnWeightedDirectedGraph
     proc initUnWeightedDirectedGraph*(N: int): UnWeightedDirectedGraph =
-        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](N))
+        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](N),N:N)
     proc add_edge*(g: var UnWeightedDirectedGraph, u, v: int) =
         g.add_edge_impl(u, v, 1, true)
 
     #UnWeightedUnDirectedGraph
     proc initUnWeightedUnDirectedGraph*(N: int): UnWeightedUnDirectedGraph =
-        result = UnWeightedUnDirectedGraph(edges: newSeq[seq[(int, int)]](N))
+        result = UnWeightedUnDirectedGraph(edges: newSeq[seq[(int, int)]](N),N:N)
     proc add_edge*(g: var UnWeightedUnDirectedGraph, u, v: int) =
         g.add_edge_impl(u, v, 1, false)
+    
+
+    proc len*(G:GraphTypes):int=G.N
