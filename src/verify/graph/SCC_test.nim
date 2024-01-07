8a35a1d070456aad8320e551295ff4c4734eefb6
GitHub Action <EMAIL>
2024-01-07 19:04:44 +0000

    [workflow] format 1809a87053f098e00b328eaceaa02ad3991bb8f6

--- src/verify/graph/SCC_test.nim
+++ src/verify/graph/SCC_test.nim
@@ -1,16 +1,16 @@
 # verification-helper: PROBLEM https://atcoder.jp/contests/abc296/submissions/49152424
 import cplib/graph/graph
 import cplib/graph/SCC
-import sequtils,math
+import sequtils, math
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
 var N = ii()
-var A = newseqwith(N,ii())
+var A = newseqwith(N, ii())
 var G = initUnWeightedDirectedGraph(N)
 var ans = 0
 for i in 0..<N:
-    G.add_edge(i,A[i]-1)
+    G.add_edge(i, A[i]-1)
     if i == A[i]-1:
         ans += 1
-var (_,_,group) = G.SCC()
-echo group.mapit(len(it)).filterIt(it>=2).sum()+ans
+var (_, _, group) = G.SCC()
+echo group.mapit(len(it)).filterIt(it >= 2).sum()+ans
