35177ad6cb0cb8d07de7ba6b6d1500f36093b1e4
GitHub Action <EMAIL>
2024-01-07 11:13:54 +0000

    [workflow] format ebfa57f3b8b8db692a44a866653c3f7eaea8c8f3

--- src/verify/graph/topologicalsort_1_test.nim
+++ src/verify/graph/topologicalsort_1_test.nim
@@ -1,19 +1,19 @@
 # verification-helper: PROBLEM https://atcoder.jp/contests/abc216/tasks/abc216_d
 import cplib/graph/graph
 import cplib/graph/topologicalsort
-import sequtils,sets
+import sequtils, sets
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
-var N,M = ii()
+var N, M = ii()
 var G = initUnWeightedDirectedGraph(N)
 for i in 0..<M:
     var K = ii()
-    var A = newseqwith(K,ii())
+    var A = newseqwith(K, ii())
     if len(A.toHashSet) != K:
         echo "No"
         quit()
     for i in 0..<K-1:
-        G.add_edge(A[i]-1,A[i+1]-1)
+        G.add_edge(A[i]-1, A[i+1]-1)
 if G.isDAG():
     echo "Yes"
 else:
--- src/verify/graph/topologicalsort_2_test.nim
+++ src/verify/graph/topologicalsort_2_test.nim
@@ -3,9 +3,9 @@ import cplib/graph/graph
 import cplib/graph/topologicalsort
 proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
 proc ii(): int {.inline.} = scanf("%lld\n", addr result)
-var N,M = ii()
+var N, M = ii()
 var G = initUnWeightedDirectedGraph(N)
 for i in 0..<M:
-    var U,V = ii()-1
-    G.add_edge(V,U)
+    var U, V = ii()-1
+    G.add_edge(V, U)
 echo N-len(G.topologicalsort())
