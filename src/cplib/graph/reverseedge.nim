87de42a7e95efcdf929abb1c9e18ef8e37535b6e
kemuniku <kemuni9@gmail.com>
2024-01-08 03:30:53 +0900

    add reverse_edge

---
+++ src/cplib/graph/reverseedge.nim
@@ -0,0 +1,13 @@
+when not declared CPLIB_GRAPH_REVERSE_EDGE:
+    const CPLIB_GRAPH_REVERSE_EDGE* = 1
+    import cplib/graph/graph
+    proc reverse_edge*[T](G:WeightedDirectedGraph[T]):WeightedDirectedGraph[T]=
+        result = WeightedDirectedGraph[T](edges: newSeq[seq[(int, T)]](G.N), N: G.N)
+        for i in 0..<G.N:
+            for (j,c) in G.edges[i]:
+                result.add_edge(j,i,c)
+    proc reverse_edge*(G:UnWeightedDirectedGraph):UnWeightedDirectedGraph=
+        result = UnWeightedDirectedGraph(edges: newSeq[seq[(int, int)]](G.N), N: G.N)
+        for i in 0..<G.N:
+            for (j,_) in G.edges[i]:
+                result.add_edge(j,i)
