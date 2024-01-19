f7b7130c0e2c2d95bf4239bd973d844773871434
seekworser <seekworser1963@gmail.com>
2024-01-20 08:48:18 +0900

    add abc335_e

---
+++ src/verify/graph/scc_abc335e_test.nim
@@ -0,0 +1,28 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc296/tasks/abc335_e
+import strutils, sequtils
+import cplib/graph/graph
+import cplib/graph/SCC
+
+
+var n, m: int
+(n, m) = stdin.readline.split.map parseInt
+var a = stdin.readLine.split.map parseInt
+var g = initUnWeightedDirectedGraph(n)
+for i in 0..<m:
+    var u, v: int
+    (u, v) = stdin.readLine.split.map parseInt
+    u -= 1; v -= 1
+    if a[u] != a[v]:
+        if a[u] > a[v]: swap(u, v)
+        g.add_edge(u, v)
+    else:
+        g.add_edge(u, v)
+        g.add_edge(v, u)
+var (newg, itg, _) = g.SCCG()
+var dp = newSeqWith(n, 0)
+dp[itg[0]] = 1
+for i in 0..<newg.N:
+    if dp[i] > 0:
+        for (j, _) in newG.edges[i]:
+            dp[j] = max(dp[j], dp[i] + 1)
+echo dp[itg[n-1]]
