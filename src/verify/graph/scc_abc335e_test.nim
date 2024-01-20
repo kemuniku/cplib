e48e08c2dd70ea5b0ca6755a1b9074d6c3607d7e
seekworser <seekworser1963@gmail.com>
2024-01-20 09:39:28 +0900

    fix typo

--- src/verify/graph/scc_abc335e_test.nim
+++ src/verify/graph/scc_abc335e_test.nim
@@ -1,4 +1,4 @@
-# verification-helper: PROBLEM https://atcoder.jp/contests/abc296/tasks/abc335_e
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc335/tasks/abc335_e
 import strutils, sequtils
 import cplib/graph/graph
 import cplib/graph/SCC
