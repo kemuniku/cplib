bde6dbecdc7108a72fecd6b96d5f5124d14660d2
GitHub Action <EMAIL>
2024-01-26 01:03:43 +0000

    [workflow] format de37f14cdd2341c576e0e9446eb363667335410a

--- src/cplib/collections/tatyamset.nim
+++ src/cplib/collections/tatyamset.nim
@@ -1,14 +1,14 @@
 # https://github.com/tatyam-prime/SortedSet/blob/main/SortedMultiset.py
 when not declared CPLIB_COLLECTIONS_TATYAMSET:
-    import algorithm,math,sequtils,sugar
+    import algorithm, math, sequtils, sugar
     const CPLIB_COLLECTIONS_TATYAMSET* = 1
-    
+
     const BUCKET_RATIO = 8
     const SPLIT_RATIO = 3
     type SortedMultiSet[T] = ref object
-        size :int
-        arr* :seq[seq[T]] 
-    proc initSortedMultiset*[T](v:seq[T]):SortedMultiSet[T]=
+        size: int
+        arr*: seq[seq[T]]
+    proc initSortedMultiset*[T](v: seq[T]): SortedMultiSet[T] =
         #Make a new SortedMultiset from seq. / O(N) if sorted / O(N log N)
         var v = v
         if not isSorted(v):
@@ -16,7 +16,7 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
         var n = len(v)
         var bucket_size = int(ceil(sqrt(n/BUCKET_RATIO)))
         var arr = collect(newseq): (for i in 0..<bucket_size: v[(n*i div bucket_size) ..< (n*(i+1) div bucket_size)])
-        result = SortedMultiSet[T](size:n,arr:arr)
+        result = SortedMultiSet[T](size: n, arr: arr)
 
     # def __iter__(self) -> Iterator[T]:
     #     for i in self.a:
@@ -25,47 +25,47 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
     # def __reversed__(self) -> Iterator[T]:
     #     for i in reversed(self.a):
     #         for j in reversed(i): yield j
-    
+
     # def __eq__(self, other) -> bool:
     #     return list(self) == list(other)
-    
-    proc len*(self:SortedMultiSet):int=
+
+    proc len*(self: SortedMultiSet): int =
         return self.size
-    
+
     # def __repr__(self) -> str:
     #     return "SortedMultiset" + str(self.a)
-    
+
     # proc `$`*(self:SortedMultiSet):string=
     #     var s = $(toseq(self))
     #     return "{" & s[1 : len(s) - 1] & "}"
 
-    proc position[T](self:SortedMultiSet[T], x: T):(int, int)=
+    proc position[T](self: SortedMultiSet[T], x: T): (int, int) =
         #"return the bucket, index of the bucket and position in which x should be. self must not be empty."
         for i, a in self.arr:
-            if x <= a[^1]: 
-                return (i,a.lowerBound(x))
+            if x <= a[^1]:
+                return (i, a.lowerBound(x))
         return (len(self.arr)-1, self.arr[^1].lowerBound(x))
 
-    proc contains*[T](self:SortedMultiSet[T], x: T):bool=
+    proc contains*[T](self: SortedMultiSet[T], x: T): bool =
         if self.size == 0: return false
         var (i, j) = self.position(x)
         return j != len(self.arr[i]) and self.arr[i][j] == x
 
-    proc incl*[T](self:SortedMultiSet[T], x: T)=
+    proc incl*[T](self: SortedMultiSet[T], x: T) =
         #"Add an element. / O(√N)"
         if self.size == 0:
             self.arr = @[@[x]]
             self.size = 1
             return
         var (b, i) = self.position(x)
-        self.arr[b].insert(x,i)
+        self.arr[b].insert(x, i)
         self.size += 1
         if len(self.arr[b]) > len(self.arr) * SPLIT_RATIO:
             var mid = len(self.arr[b]) shr 1
-            self.arr.insert(self.arr[b][mid..<len(self.arr[b])],b+1)
+            self.arr.insert(self.arr[b][mid..<len(self.arr[b])], b+1)
             self.arr[b] = self.arr[b][0..<mid]
 
-    proc innerpop[T](self:SortedMultiSet[T], b: int, i: int):T=
+    proc innerpop[T](self: SortedMultiSet[T], b: int, i: int): T =
         var b = b
         if b < 0:
             b = self.size + b
@@ -74,7 +74,7 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
         if len(self.arr) == 0: self.a.delete(b)
         return ans
 
-    proc excl*[T](self:SortedMultiSet[T], x: T):bool{.discardable.}=
+    proc excl*[T](self: SortedMultiSet[T], x: T): bool{.discardable.} =
         #"Remove an element and return True if removed. / O(√N)"
         if self.size == 0: return false
         var (b, i) = self.position(x)
@@ -82,38 +82,38 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
         self.innerpop(b, i)
         return true
 
-    proc lt*[T](self:SortedMultiSet[T], x: T):T=
+    proc lt*[T](self: SortedMultiSet[T], x: T): T =
         #"Find the largest element < x, or None if it doesn't exist."
-        for i in countdown(len(self.arr)-1,0,1):
+        for i in countdown(len(self.arr)-1, 0, 1):
             if self.arr[i][0] < x:
                 return self.arr[i][lowerBound(self.arr[i], x) - 1]
         return nil
 
-    proc le*[T](self:SortedMultiSet[T], x: T):T=
+    proc le*[T](self: SortedMultiSet[T], x: T): T =
         #"Find the largest element <= x, or None if it doesn't exist."
-        for i in countdown(len(self.arr)-1,0,1):
+        for i in countdown(len(self.arr)-1, 0, 1):
             if self.arr[i][0] <= x:
                 return self.arr[i][upperBound(self.arr[i], x) - 1]
         return nil
 
-    proc gt*[T](self:SortedMultiSet[T], x: T):T=
+    proc gt*[T](self: SortedMultiSet[T], x: T): T =
         #"Find the smallest element > x, or None if it doesn't exist."
         for i in 0..<len(self.arr):
             if self.arr[i][^1] > x:
                 return self.arr[i][upperBound(self.arr[i], x)]
         return nil
 
-    proc ge*[T](self:SortedMultiSet[T], x: T):T=
+    proc ge*[T](self: SortedMultiSet[T], x: T): T =
         #"Find the smallest element >= x, or None if it doesn't exist."
         for i in 0..<len(self.arr):
             if self.arr[i][^1] >= x:
                 return self.arr[i][lowerBound(self.arr[i], x)]
         return nil
-    
-    proc `[]`*[T](self:SortedMultiSet[T], i: int):T=
+
+    proc `[]`*[T](self: SortedMultiSet[T], i: int): T =
         #"Return the i-th element."
         if i < 0:
-            for j in countdown(len(self.arr)-1,0,1):
+            for j in countdown(len(self.arr)-1, 0, 1):
                 i += len(self.arr[j])
                 if i >= 0: return self.arr[j][i]
         else:
@@ -121,11 +121,11 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
                 if i < len(self.arr[j]): return self.arr[j][i]
                 i -= len(self.arr[j])
         raise newException(IndexDefect, "index " & $i & " not in 0 .. " & $self.size-1)
-    
-    proc pop*[T](self:SortedMultiSet[T], i: int = -1):T=
+
+    proc pop*[T](self: SortedMultiSet[T], i: int = -1): T =
         #"Pop and return the i-th element."
         if i < 0:
-            for b in countdown(len(self.arr)-1,0,1):
+            for b in countdown(len(self.arr)-1, 0, 1):
                 i += len(self.arr[b])
                 if i >= 0: return self.innerpop(not b, i)
         else:
@@ -134,19 +134,19 @@ when not declared CPLIB_COLLECTIONS_TATYAMSET:
                 i -= len(self.arr[b])
         raise newException(IndexDefect, "index " & $i & " not in 0 .. " & $self.size-1)
 
-    proc index*[T](self:SortedMultiSet[T], x: T):int=
+    proc index*[T](self: SortedMultiSet[T], x: T): int =
         #"Count the number of elements < x."
         for i in 0..<len(self.arr):
             if self.arr[i][^1] >= x:
                 return result + lowerBound(self.arr[i], x)
             result += len(self.arr[i])
 
-    proc index_right*[T](self:SortedMultiSet[T], x: T):int=
+    proc index_right*[T](self: SortedMultiSet[T], x: T): int =
         #"Count the number of elements <= x."
         for i in 0..<len(self.arr):
             if self.arr[i][^1] > x:
                 return result + upperBound(self.arr[i], x)
             result += len(self.arr[i])
-    proc count*[T](self:SortedMultiSet[T], x: T):int=
+    proc count*[T](self: SortedMultiSet[T], x: T): int =
         #"Count the number of x."
         return self.index_right(x) - self.index(x)
