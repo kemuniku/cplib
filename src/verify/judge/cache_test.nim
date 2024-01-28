b2176f212761421210316f543feb2d8cf1e0c8b1
seekworser <seekworser1963@gmail.com>
2024-01-28 10:24:45 +0900

    delete test file

--- src/verify/judge/cache_test.nim
+++
@@ -1,7 +0,0 @@
-# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_B
-import strscans, std/math
-
-var x, y: int
-discard stdin.readLine.scanf("$i $i", x, y)
-discard x
-echo gcd(x, y)
