16ac7f5bae899b844ba7e3b5af24c671c7c6fefb
seekworser <seekworser1963@gmail.com>
2024-02-07 11:08:04 +0900

    minor fix, add unittest

--- src/cplib/collections/defaultdict.nim
+++ src/cplib/collections/defaultdict.nim
@@ -5,7 +5,7 @@ when not declared CPLIB_COLLECTIONS_DEFAULTDICT:
         table: Table[K, V]
         default: V
     proc initDefaultDict*[K, V](default: V): DefaultDict[K, V] = DefaultDict[K, V](table: initTable[K, V](), default: default)
-    proc `==`*[K, V](src, dst: DefaultDict[K, V]) = src.table == dst.table
+    proc `==`*[K, V](src, dst: DefaultDict[K, V]): bool = src.table == dst.table
     proc `[]=`*[K, V](d: var DefaultDict[K, V], key: K, val: V) = d.table[key] = val
     proc `[]`*[K, V](d: DefaultDict[K, V], key: K): V =
         if key notin d.table: return d.default()
@@ -14,13 +14,13 @@ when not declared CPLIB_COLLECTIONS_DEFAULTDICT:
         if key notin d.table: d.table[key] = d.default
         return d.table[key]
     proc clear*[K, V](d: var DefaultDict[K, V]) = d.table = initTable[int, int](0)
-    proc contains*[K, V](d: var DefaultDict[K, V], key: K) = d.table.contains(key)
+    proc contains*[K, V](d: var DefaultDict[K, V], key: K): bool = d.table.contains(key)
     proc del*[K, V](d: var DefaultDict[K, V], key: K) = d.table.del(key)
     proc hash*[K, V](d: DefaultDict[K, V]): Hash = d.table.hash
     proc hasKey*[K, V](d: DefaultDict[K, V], key: K): bool = d.table.hasKey(key)
     proc len*[K, V](d: DefaultDict[K, V]): int = d.table.len
-    proc pop*[K, V](d: var DefaultDict, key: K, val: var V) = d.table.pop(key, val)
-    proc take*[K, V](d: var DefaultDict, key: K, val: var V) = d.table.pop(key, val)
+    proc pop*[K, V](d: var DefaultDict, key: K, val: var V): bool = d.table.pop(key, val)
+    proc take*[K, V](d: var DefaultDict, key: K, val: var V): bool = d.table.pop(key, val)
     proc toDefaultDict*[K, V](pairs: openArray[(K, V)], default: V): DefaultDict[K, V] =
         result = initDefaultDict[K, V](default)
         result.table = pairs.toTable
---
+++ src/verify/collections/defaultdict/defaultdict_abc235c_test.nim
@@ -0,0 +1,16 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc278/tasks/abc278_c
+import sequtils, strutils
+include cplib/collections/defaultdict
+
+var n, q, x, k: int
+(n, q) = stdin.readLine.split.map parseInt
+var d = initDefaultDict[int, seq[int]](newSeq[int](0))
+var a = stdin.readLine.split.map parseInt
+for i in 0..<n:
+    d[a[i]].add(i+1)
+var ans = newSeq[int](q)
+for i in 0..<q:
+    (x, k) = stdin.readLine.split.map parseInt
+    if d[x].len < k: ans[i] = -1
+    else: ans[i] = d[x][k-1]
+echo ans.join("\n")
