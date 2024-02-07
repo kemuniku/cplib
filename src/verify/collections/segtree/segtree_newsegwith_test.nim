628ef5a334b38e03cdc79bc245b42d1cae96183f
GitHub Action <EMAIL>
2024-02-07 20:20:25 +0000

    [workflow] format 3444d48c02607985d37db89443bf0f96ba6e0353

--- src/verify/collections/segtree/segtree_newsegwith_test.nim
+++ src/verify/collections/segtree/segtree_newsegwith_test.nim
@@ -7,7 +7,7 @@ proc ii(): int {.inline.} = scanf("%lld\n", addr result)
 
 var N, Q = ii()
 var A = newSeqWith(N, ii())
-var st = newSegWith(A,min(l,r), int(1e18))
+var st = newSegWith(A, min(l, r), int(1e18))
 for i in 0..<Q:
     var L, R = ii()
     echo st.get(L, R)
