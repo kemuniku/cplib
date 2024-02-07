3c6422db16a80f9b2d4b3038c1efb07a8a2d0482
GitHub Action <EMAIL>
2024-02-07 16:25:18 +0000

    [workflow] format b8c7650349164646b78c99f642362ef826d9d8e8

--- src/cplib/math/isqrt.nim
+++ src/cplib/math/isqrt.nim
@@ -1,6 +1,6 @@
 when not declared CPLIB_MATH_ISQRT:
     const CPLIB_MATH_ISQRT* = 1
-    proc isqrt*(n:int):int=
+    proc isqrt*(n: int): int =
         var x = n
         var y = (x + 1) shr 1
         while y < x:
--- src/verify/math/isqrt_test.nim
+++ src/verify/math/isqrt_test.nim
@@ -8,10 +8,10 @@ var D = ii()
 var ans = D
 for x in 0..(1 + isqrt(D - 1)):
     if (x^2-D) >= 0:
-        ans = min(ans,x^2-D)
+        ans = min(ans, x^2-D)
     else:
         var y1 = isqrt(D-x^2)
         var y2 = 1 + y1
-        ans = min(ans,abs(x^2+y1^2-D))
-        ans = min(ans,abs(x^2+y2^2-D))
+        ans = min(ans, abs(x^2+y1^2-D))
+        ans = min(ans, abs(x^2+y2^2-D))
 echo ans
