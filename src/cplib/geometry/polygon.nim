6a16bcb32d0c319c6474547969e6cfc6351e397d
seekworser <seekworser1963@gmail.com>
2024-03-20 02:34:31 +0900

    add isconvex

--- src/cplib/geometry/polygon.nim
+++ src/cplib/geometry/polygon.nim
@@ -3,6 +3,7 @@ when not declared CPLIB_GEOMETRY_POLYGON:
     import cplib/geometry/base
     import cplib/geometry/ccw
     import cplib/math/fractions
+    import algorithm
     type Polygon*[T] = object
         v: seq[Point[T]]
 
@@ -14,3 +15,14 @@ when not declared CPLIB_GEOMETRY_POLYGON:
         for i in 0..<p.v.len: result += cross(p.v[i], p.v[(i+1) mod p.v.len]) / 2
     proc area*(p: Polygon[float]): float =
         for i in 0..<p.v.len: result += cross(p.v[i], p.v[(i+1) mod p.v.len]) / 2.0
+
+    proc is_convex_ccw*[T](poly: Polygon[T], strict: bool = true): bool =
+        for i in 0..<poly.v.len:
+            var ccwi = ccw(poly.v[i], poly.v[(i+1) mod poly.v.len], poly.v[(i+2) mod poly.v.len])
+            if strict and ccwi != COUNTER_CLOCKWISE: return false
+            if (not strict) and ccwi == CLOCKWISE: return false
+        return true
+    proc is_convex*[T](poly: Polygon[T], strict: bool = true): bool =
+        if is_convex_ccw(poly, strict): return true
+        var pn = Polygon[T](v: poly.v.reversed)
+        return is_convex_ccw(pn, strict)
