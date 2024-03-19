b518f00dc0bff3cda2fe3908404709f6de13dbcc
seekworser <seekworser1963@gmail.com>
2024-03-19 10:12:38 +0900

    fix dir

---
+++ src/verify/geometry/CGL_2/cross_point_fraction_cgl2c_test.nim
@@ -0,0 +1,13 @@
+# verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/1/CGL_2_B
+include cplib/geometry/intersect
+import cplib/math/fractions
+import strformat
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+
+var q = ii()
+proc get(): Point[Fraction[int]] = initPoint(initFraction(ii()), initFraction(ii()))
+for _ in 0..<q:
+    var l1, l2 = initLine(get(), get())
+    var p = cross_point(l1, l2)
+    echo &"{p.x.toFloat:.10f} {p.y.toFloat:.10f}"
