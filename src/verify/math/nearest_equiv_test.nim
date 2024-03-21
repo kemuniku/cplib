2db9b3fe5f8f0920bb372cdfa3e6274b0b01fa0c
seekworser <seekworser1963@gmail.com>
2024-03-21 23:27:29 +0900

    add verify

---
+++ src/verify/math/nearest_equiv_test.nim
@@ -0,0 +1,9 @@
+# verification-helper: PROBLEM https://yukicoder.me/problems/no/2686
+import strutils, sequtils
+import cplib/math/nearest_equiv
+
+var a, m, l, r: int
+(a, m, l, r) = stdin.readLine.split.map(parseInt)
+var s = nearest_equiv(a, l, m)
+var t = nearest_equiv(a, r+1, m)
+echo max(0, (t - s) div m)
