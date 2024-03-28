65aba5238b55cb6380c084b8679748611246c9f4
seekworser <seekworser1963@gmail.com>
2024-03-28 20:13:34 +0900

    add verify

--- src/verify/matrix/matrix_unit_test.nim
+++ src/verify/matrix/matrix_unit_test.nim
@@ -1,6 +1,6 @@
 # verification-helper: PROBLEM https://onlinejudge.u-aizu.ac.jp/problems/ITP1_1_A
 echo "Hello World"
-import sequtils, math, sets, algorithm
+import sequtils, strutils, sets
 import cplib/matrix/matrix
 import cplib/matrix/matops
 import random
@@ -8,12 +8,14 @@ import random
 var rng = initRand(0)
 
 var l, r = newSeq[Matrix[int]]()
-var a = newSeqWith(2, newSeqWith(3, rng.rand(-5..5)))
-r.add(initMatrix(a))
-l.add(initMatrix(a.transposed))
-a = newSeqWith(2, newSeqWith(3, rng.rand(-5..5)))
-r.add(a.toMatrix)
-r.add(a.transposed.toMatrix)
+for i in 0..<2:
+    var a = newSeqWith(2, newSeqWith(3, rng.rand(-5..5)))
+    l.add(initMatrix(a))
+    r.add(initMatrix(a.transposed))
+for i in 0..<2:
+    var a = newSeqWith(2, newSeqWith(3, rng.rand(-5..5)))
+    l.add(a.toMatrix)
+    r.add(a.transposed.toMatrix)
 for i in -1..1:
     l.add(initMatrix(2, 3, i))
     r.add(initMatrix(3, 2, i))
@@ -22,78 +24,68 @@ var r3 = initMatrix((1..3).toSeq)
 var c2 = initMatrix((1..2).toSeq, true)
 var c3 = initMatrix((1..3).toSeq, true)
 
+assert l.mapIt(it.h).allIt(it == 2) and l.mapIt(it.w).allIt(it == 3)
+assert r.mapIt(it.h).allIt(it == 3) and r.mapIt(it.w).allIt(it == 2)
+assert r2.h == 1 and r2.w == 2
+assert r3.h == 1 and r3.w == 3
+assert c2.h == 2 and c2.w == 1
+assert c3.h == 3 and c3.w == 1
+
+
+template assert_op(op1, op2: untyped) =
+    for lr in [l, r]:
+        for a in lr:
+            for b in lr:
+                check(op1, op2, a, b)
+template check(op1, op2, a, b: untyped) =
+    var m1 = op1(a, b)
+    var m2 = a
+    op2(m2, b)
+    for i in 0..<a.h:
+        for j in 0..<a.w:
+            assert m1[i][j] == m2[i, j]
+assert_op(`+`, `+=`)
+assert_op(`-`, `-=`)
+assert_op(`*`, `*=`)
+assert_op(`and`, `and=`)
+assert_op(`or`, `or=`)
+assert_op(`xor`, `xor=`)
+check(`shl`, `shl=`, l[^1], l[^1])
+check(`shr`, `shr=`, l[^1], l[^1])
+check(`div`, `div=`, l[^1], l[^1])
+check(`mod`, `mod=`, l[^1], l[^1])
+
+for item in l:
+    assert item == item
+    assert $item == $item
+    assert hash(item) == hash(item)
+    assert -item + 10 != item
 for item in r:
-    echo item
-    echo "---"
+    assert item == item
+    assert $item == $item
+    assert -item + 10 != item
 
+var st = initHashSet[Matrix[int]]()
+for item in l: st.incl(item)
+for item in r: st.incl(item)
+assert st.len == 14
 
-# template assert_op(op1, op2: untyped) =
-#     var x = initFraction(1, 0)
-#     for i in r:
-#         for j in r:
-#             if isNaN(x): x = initFraction(1, 0)
-#             var xi = x
-#             op2(x, i)
-#             assert op1(xi, i) == x or isNaN(x)
-#             assert (abs(op1(i, j).tofloat - op1(i.tofloat, j.tofloat)) <= 1e-10) or (op1(i.toFloat, j.toFloat).abs.classify == fcInf) or (op1(i.toFloat, j.toFloat).abs.classify == fcNan)
-#     x = initFraction(1, 0)
-#     for i in r:
-#         for j in t:
-#             if isNaN(x): x = initFraction(1, 0)
-#             var xi = x
-#             op2(x, i)
-#             assert op1(xi, i) == x or isNaN(x)
-#             assert (abs(op1(i, j).tofloat - op1(i.tofloat, j.tofloat)) <= 1e-10) or (op1(i.toFloat, j.toFloat).abs.classify == fcInf) or (op1(i.toFloat, j.toFloat).abs.classify == fcNan)
-#     for i in t:
-#         for j in r:
-#             assert (abs(op1(i, j).tofloat - op1(i.tofloat, j.tofloat)) <= 1e-10) or (op1(i.toFloat, j.toFloat).abs.classify == fcInf) or (op1(i.toFloat, j.toFloat).abs.classify == fcNan)
-# assert_op(`+`, `+=`)
-# assert_op(`-`, `-=`)
-# assert_op(`*`, `*=`)
-# assert_op(`/`, `/=`)
+var iden = identity_matrix[int](3)
+for i in 0..<3:
+    for j in 0..<3:
+        if i == j: assert iden[i, j] == 1
+        else: assert iden[i, j] == 0
+for i in [0, 1, 2, 10, 100, 1000]:
+    assert iden.pow(i) == iden
+assert iden.sum == 3
 
-# proc assert_cmp() =
-#     proc eq(x, y: float): bool = (x == y) or (abs(x - y) < 1e-10)
-#     proc lt(x, y: float): bool = (not eq(x, y)) and (x < y)
-#     proc gt(x, y: float): bool = (not eq(x, y)) and (x > y)
-#     proc cmp(x, y: float): int = (if lt(x, y): -1 elif eq(x, y): 0 else: 1)
-#     for i in r:
-#         for j in r:
-#             assert lt(i.toFloat, j.toFloat) == (i < j)
-#             assert gt(i.toFloat, j.toFloat) == (i > j)
-#             assert eq(i.toFloat, j.toFloat) == (i == j)
-#             assert (not lt(i.toFloat, j.toFloat)) == (i >= j)
-#             assert (not gt(i.toFloat, j.toFloat)) == (i <= j)
-#             assert (not eq(i.toFloat, j.toFloat)) == (i != j)
-#             assert cmp(i.toFloat, j.toFloat) == cmp(i, j)
-#     for i in r:
-#         for j in t:
-#             assert lt(i.toFloat, j.toFloat) == (i < j)
-#             assert gt(i.toFloat, j.toFloat) == (i > j)
-#             assert eq(i.toFloat, j.toFloat) == (i == j)
-#             assert (not lt(i.toFloat, j.toFloat)) == (i >= j)
-#             assert (not gt(i.toFloat, j.toFloat)) == (i <= j)
-#             assert (not eq(i.toFloat, j.toFloat)) == (i != j)
-#             assert cmp(i.toFloat, j.toFloat) == cmp(i, j)
-#     for i in t:
-#         for j in r:
-#             assert lt(i.toFloat, j.toFloat) == (i < j)
-#             assert gt(i.toFloat, j.toFloat) == (i > j)
-#             assert eq(i.toFloat, j.toFloat) == (i == j)
-#             assert (not lt(i.toFloat, j.toFloat)) == (i >= j)
-#             assert (not gt(i.toFloat, j.toFloat)) == (i <= j)
-#             assert (not eq(i.toFloat, j.toFloat)) == (i != j)
-#             assert cmp(i.toFloat, j.toFloat) == cmp(i, j)
-# assert_cmp()
-
-# var st = initHashSet[Fraction[int]](0)
-# for _ in 0..1:
-#     for ri in r:
-#         st.incl(ri)
-# for ri in r:
-#     assert ri.inv == (1 / ri)
-#     assert ri.abs == (if ri < 0: -ri else: ri)
-# assert st.len == r.sorted.deduplicate(true).len
-
-# var c: Fraction[int] = 2
-# assert c == initFraction(2, 1)
+for a in l:
+    for b in r:
+        var m1 = a @ b
+        var m2 = a
+        m2 @= b
+        assert m1 == m2
+    var m3 = r2 @ a
+    var m4 = a @ c3
+    assert m3.h == 1 and m3.w == 3
+    assert m4.h == 2 and m4.w == 1
