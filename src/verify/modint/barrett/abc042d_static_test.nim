0ee016958884a9a613d23325e915f1b52cd5f3c2
seekworser <seekworser1963@gmail.com>
2024-04-08 02:16:36 +0900

    add barrett

---
+++ src/verify/modint/barrett/abc042d_static_test.nim
@@ -0,0 +1,14 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc042/tasks/arc058_b
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+import cplib/modint/modint
+import cplib/math/combination
+type mint = modint1000000007_barrett
+
+var h, w, a, b = ii()
+var c = initCombination[mint](h+w)
+proc get(h, w: int): mint = c.ncr(h+w-2, h-1)
+var ans = mint(0)
+for i in b+1..w:
+    ans += get(h-a, i) * get(a, w-i+1)
+echo ans
