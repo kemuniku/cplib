0bd0a5ec38883c350127b0452cf025e1259748c6
seekworser <seekworser1963@gmail.com>
2024-04-11 04:28:13 +0900

    add grid_to_graph verify

---
+++ src/verify/graph/dynamic/grid_to_graph_abc151d_test.nim
@@ -0,0 +1,17 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc151/tasks/abc151_d
+import sequtils
+import cplib/graph/graph
+import cplib/graph/grid_to_graph
+import cplib/graph/maxk_dijkstra
+import cplib/utils/infl
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+
+var h, w = ii()
+var g = newSeqWith(h, stdin.readLine).grid_to_graph('.', false)
+var ans = 0
+for sx in 0..<h:
+    for sy in 0..<w:
+        var d = g.maxk_dijkstra(sx*w+sy, 1).mapIt(if it == INFL: -1 else: it)
+        ans = max(ans, d.max)
+echo ans
