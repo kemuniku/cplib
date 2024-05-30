6f061c9e03137d1f4d8641939d72d9af125c9002
kemuniku <kemuni9@gmail.com>
2024-05-30 18:26:36 +0900

    add matrix_product

---
+++ src/verify/matrix/matrix_product_test.nim
@@ -0,0 +1,14 @@
+import cplib/modint/modint
+import cplib/matrix/matrix
+import sequtils,strutils
+
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+
+type mint = modint998244353_montgomery
+var N,M,K = ii()
+var A = newSeqWith(N,newSeqWith(M,mint(ii()))).toMatrix()
+var B = newSeqWith(M,newSeqWith(K,mint(ii()))).toMatrix()
+var C = A*B
+for i in 0..<N:
+    echo C[i].join(" ")
