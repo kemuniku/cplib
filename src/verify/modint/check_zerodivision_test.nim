be99379c37fd01d0d521096d6000af1cf7810103
seekworser <seekworser1963@gmail.com>
2024-07-08 10:51:59 +0900

    fix verify

--- src/verify/modint/check_zerodivision_test.nim
+++ src/verify/modint/check_zerodivision_test.nim
@@ -4,22 +4,24 @@ echo "Hello World"
 import unittest
 import cplib/modint/modint
 
-suite "test zero division":
-    test "Dynamic Montogomery Zero Division Test":
-        type mint = modint_montgomery
-        mint.setMod(998244353)
-        expect(Defect):
-            discard mint(0).inv
-    test "Static Montogomery Zero Division Test":
-        type mint = modint998244353_montgomery
-        expect(Defect):
-            discard mint(0).inv
-    test "Dynamic Barrett Zero Division Test":
-        type mint = modint_barrett
-        mint.setMod(998244353)
-        expect(Defect):
-            discard mint(0).inv
-    test "Static Barrett Zero Division Test":
-        type mint = modint998244353_barrett
-        expect(Defect):
-            discard mint(0).inv
+addOutputFormatter(newConsoleOutputFormatter(OutputLevel.PRINT_FAILURES))
+
+test "Dynamic Montogomery Zero Division Test":
+    type mint = modint_montgomery
+    mint.setMod(998244353)
+    expect(Defect):
+        discard mint(0).inv
+test "Static Montogomery Zero Division Test":
+    type mint = modint998244353_montgomery
+    expect(Defect):
+        discard mint(0).inv
+test "Dynamic Barrett Zero Division Test":
+    type mint = modint_barrett
+    mint.setMod(998244353)
+    expect(Defect):
+        discard mint(0).inv
+test "Static Barrett Zero Division Test":
+    type mint = modint998244353_barrett
+    expect(Defect):
+        discard mint(0).inv
+
