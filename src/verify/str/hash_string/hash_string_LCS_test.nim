6e6c7b91d7e6f258520d3fa3e262c91aba4891a8
kemuniku <kemuni9@gmail.com>
2024-07-18 19:41:30 +0900

    fix verify

--- src/verify/str/hash_string/hash_string_LCS_test.nim
+++ src/verify/str/hash_string/hash_string_LCS_test.nim
@@ -1,4 +1,4 @@
-# verification-helper: PROBLEM https://judge.yosupo.jp/problem/number_of_substrings
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/longest_common_substring
 include cplib/str/hash_string
 
 {.checks:off.}
@@ -11,8 +11,23 @@ for i in 0..<len(X):
     tmp.add(X[i..<len(X)])
 tmp.sort()
 var ans = 0
+var a = 0
+var b = 0
+var c = 0
+var d = 0
 for i in 0..<(len(X)-1):
     if (tmp[i].l in 0..<len(S) and tmp[i+1].l notin 0..<len(S)) or (tmp[i+1].l in 0..<len(S) and tmp[i].l notin 0..<len(S)):
-        if ans < LCP(tmp[i],tmp[i+1]):
-            ans = LCP(tmp[i],tmp[i+1])
-echo ans
+        var lcp = LCP(tmp[i],tmp[i+1])
+        if ans < lcp:
+            ans = lcp
+            if tmp[i].l in 0..<len(S):
+                a = tmp[i].l
+                b = tmp[i].l+lcp
+                c = tmp[i+1].l-len(S)-1
+                d = tmp[i+1].l+lcp-len(S)-1
+            else:
+                a = tmp[i+1].l
+                b = tmp[i+1].l+lcp
+                c = tmp[i].l-len(S)-1
+                d = tmp[i].l+lcp-len(S)-1
+echo a," ",b," ",c," ",d
