bc17775815649b95d845a9d69ba1213369aa05b9
seekworser <seekworser1963@gmail.com>
2024-09-19 02:01:29 +0900

    improve speed of verify

--- src/verify/collections/lazysegtree/rangesetrangecomposite_test.nim
+++ src/verify/collections/lazysegtree/rangesetrangecomposite_test.nim
@@ -1,5 +1,5 @@
 # verification-helper: PROBLEM https://judge.yosupo.jp/problem/range_set_range_composite
-import sequtils
+import sequtils, strutils
 import cplib/collections/lazysegtree
 import cplib/modint/modint
 
@@ -13,11 +13,16 @@ type S = (mint, mint, int)
 type F = (bool, mint, mint)
 var v = newSeqWith(N, (mint(ii()), mint(ii()), 1))
 proc op(l, r: S): S = (r[0] * l[0], r[0] * l[1] + r[1], l[2] + r[2])
-proc mapping(f: F, x: S): S = (if f[0]: (f[1].pow(x[2]), f[2] * (1 - f[1].pow(x[2])) / (1 - f[1]), x[2]) else: x)
+proc mapping(f: F, x: S): S =
+    if f[0]:
+        var pw = f[1].pow(x[2])
+        (pw, f[2] * (1 - pw) / (1 - f[1]), x[2])
+    else:
+        x
 proc composition(f: F, g: F): F = (if f[0]: f else: g)
 var seg = initLazySegmentTree(v, op, (mint(1), mint(0), 0), mapping, composition, (false, mint(1), mint(0)))
 
-
+var ans = newSeq[mint]()
 for i in 0..<Q:
     var t = ii()
     if t == 0:
@@ -26,4 +31,5 @@ for i in 0..<Q:
     else:
         var l, r, x = ii()
         var (a, b, _) = seg[l..<r]
-        echo a * x + b
+        ans.add(a * x + b)
+echo ans.join("\n")
