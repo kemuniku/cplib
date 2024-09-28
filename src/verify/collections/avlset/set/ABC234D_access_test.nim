049f1a9b73c05dd86400f3631b9413dd6485e3d2
kemuniku <kemuni9@gmail.com>
2024-09-28 12:27:57 +0900

    add verify

---
+++ src/verify/collections/avlset/set/ABC234D_access_test.nim
@@ -0,0 +1,14 @@
+# verification-helper: PROBLEM https://atcoder.jp/contests/abc234/tasks/abc234_d
+proc scanf(formatstr: cstring){.header: "<stdio.h>", varargs.}
+proc ii(): int {.inline.} = scanf("%lld\n", addr result)
+import sequtils, strutils
+import cplib/collections/avlset
+var N, K = ii()
+var P = newseqwith(N, ii())
+K -= 1
+var s = initAvlSortedSet(P[0..<K])
+var ans: seq[int]
+for i in K..<N:
+    s.incl(P[i])
+    ans.add(s[^(K+1)])
+echo ans.join("\n")
