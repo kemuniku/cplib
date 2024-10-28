b72885c431b1caca4df426c68d0da7bc1cd8a2d5
kemuniku <kemuni9@gmail.com>
2024-10-29 00:40:30 +0900

    verify akirame

--- src/verify/str/lcs_test.nim
+++
@@ -1,8 +0,0 @@
-# verification-helper: PROBLEM https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_t
-
-import sequtils,strutils
-import cplib/str/lcs
-
-var S = stdin.readline().toseq()
-var T = stdin.readline().toseq()
-echo LCS(S,T)
