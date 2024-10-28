775fd8f83bc33dd815a98493d5f0aaecaa0bcb3a
kemuniku <kemuni9@gmail.com>
2024-10-29 00:37:30 +0900

    change verify

--- src/verify/str/lcs_test.nim
+++ src/verify/str/lcs_test.nim
@@ -1,9 +1,8 @@
-# verification-helper: PROBLEM https://atcoder.jp/contests/dp/tasks/dp_f
+# verification-helper: PROBLEM https://atcoder.jp/contests/tessoku-book/tasks/tessoku_book_t
 
 import sequtils,strutils
 import cplib/str/lcs
 
 var S = stdin.readline().toseq()
 var T = stdin.readline().toseq()
-echo restoreLCS(S,T).join("")
-assert len(restoreLCS(S,T)) == LCS(S,T)
+echo LCS(S,T)
