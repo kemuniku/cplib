a6275e8f94ef36c3b5a5b74819aa68d900b7edaf
seekworser <seekworser1963@gmail.com>
2025-04-27 18:32:02 +0900

    make avltreenode nilnode to nil

--- src/cplib/collections/avltreenode_old.nim
+++ src/cplib/collections/avltreenode_old.nim
@@ -1,38 +1,39 @@
 when not declared CPLIB_COLLECTIONS_AVLTREE:
     const CPLIB_COLLECTIONS_AVLTREE* = 1
+    when compileOption("mm", "orc") or compileOption("mm", "arc"):
+        {.fatal: "Plese Use refc".}
     # 以下をNimに移植
     # https://nachiavivias.github.io/cp-library/cpp/array/bbst-list.html
     type AvlTreeNode*[K] = ref object
         l*, r*, p*: AvlTreeNode[K]
         h*, len*: int
         key*: K
+    proc get_avltree_nilnode*[K](): AvlTreeNode[K] =
+        let nil_node {.global.} = (block:
+            var nil_node = AvlTreeNode[K](h: 0, len: 0)
+            nil_node.l = nil_node
+            nil_node.r = nil_node
+            nil_node.p = nil_node
+            nil_node
+        )
+        return nil_node
     proc update[K](node: AvlTreeNode[K]) =
-        node.h = 0
-        node.len = 1
-        if not node.l.isNil:
-            if node.l.h + 1 > node.h: node.h = node.l.h + 1
-            node.len += node.l.len
-        if not node.r.isNil:
-            if node.r.h + 1 > node.h: node.h = node.r.h + 1
-            node.len += node.r.len
+        node.h = max(node.l.h + 1, node.r.h + 1)
+        node.len = 1 + node.l.len + node.r.len
     proc set_children[K](node, l, r: AvlTreeNode[K]) =
         node.l = l
-        if not l.isNil: l.p = node
+        if not l.isNil and l != get_avltree_nilnode[K](): l.p = node
         node.r = r
-        if not r.isNil: r.p = node
+        if not r.isNil and r != get_avltree_nilnode[K](): r.p = node
         node.update()
     proc rebalance[K](node: AvlTreeNode[K]): AvlTreeNode[K] =
         var node = node
         var l = node.l
         var r = node.r
-        var lh = if not l.isNil: l.h else: 0
-        var rh = if not r.isNil: r.h else: 0
-        if lh + 1 < rh:
+        if l.h + 1 < r.h:
             var rl = r.l
             var rr = r.r
-            var rlh = if not rl.isNil: rl.h else: 0
-            var rrh = if not rr.isNil: rr.h else: 0
-            if rlh <= rrh:
+            if rl.h <= rr.h:
                 r.p = node.p
                 node.set_children(l, rl)
                 r.set_children(node, rr)
@@ -43,12 +44,10 @@ when not declared CPLIB_COLLECTIONS_AVLTREE:
                 r.set_children(rl.r, rr)
                 rl.set_children(node, r)
                 return rl
-        elif rh + 1 < lh:
+        elif r.h + 1 < l.h:
             var ll = l.l
             var lr = l.r
-            var llh = if not ll.isNil: ll.h else: 0
-            var lrh = if not lr.isNil: lr.h else: 0
-            if lrh <= llh:
+            if lr.h <= ll.h:
                 l.p = node.p
                 node.set_children(lr, r)
                 l.set_children(ll, node)
@@ -63,7 +62,7 @@ when not declared CPLIB_COLLECTIONS_AVLTREE:
         return node
     proc rebalance_to_root[K](node: AvlTreeNode[K]): AvlTreeNode[K] =
         var node = node
-        while not node.p.isNil:
+        while not node.p.isNil and node.p != get_avltree_nilnode[K]():
             var cp = node.p
             if cp.l == node: cp.l = node.rebalance
             else: cp.r = node.rebalance
@@ -71,12 +70,12 @@ when not declared CPLIB_COLLECTIONS_AVLTREE:
         return node.rebalance
     proc rootOf*[K](node: AvlTreeNode[K]): AvlTreeNode[K] =
         result = node
-        while not result.p.isNil: result = result.p
+        while result.p != get_avltree_nilnode[K](): result = result.p
     proc node_search[K](node: AvlTreeNode[K], key: K, strict: bool): (AvlTreeNode[K], AvlTreeNode[K]) =
         var node = node
-        var result_l: AvlTreeNode[K] = nil
-        var result_r: AvlTreeNode[K] = nil
-        while not node.isNil:
+        var result_l = get_avltree_nilnode[K]()
+        var result_r = get_avltree_nilnode[K]()
+        while node != get_avltree_nilnode[K]():
             if (strict and key < node.key) or (not strict and key <= node.key):
                 result_r = node
                 node = node.l
@@ -87,79 +86,79 @@ when not declared CPLIB_COLLECTIONS_AVLTREE:
     proc lower_bound_node*[K](node: AvlTreeNode[K], key: K): (AvlTreeNode[K], AvlTreeNode[K]) = node_search[K](node, key, false)
     proc upper_bound_node*[K](node: AvlTreeNode[K], key: K): (AvlTreeNode[K], AvlTreeNode[K]) = node_search[K](node, key, true)
     proc insert*[K](node, x: AvlTreeNode[K]): AvlTreeNode[K] =
-        if node.isNil: return x
+        if node == get_avltree_nilnode[K](): return x
         var (ql, qr) = node.lower_bound_node(x.key)
-        if not ql.isNil and ql.r.isNil:
+        if ql != get_avltree_nilnode[K]() and ql.r == get_avltree_nilnode[K]():
             ql.set_children(ql.l, x)
             return ql.rebalance_to_root
         qr.set_children(x, qr.r)
         return qr.rebalance_to_root
     proc erase*[K](node, x, nxt: AvlTreeNode[K]): AvlTreeNode[K] =
         var xp = x.p
-        if x.r.isNil:
+        result = get_avltree_nilnode[K]()
+        if x.r == get_avltree_nilnode[K]():
             var xl = x.l
-            if not xl.isNil: xl.p = xp
-            if not xp.isNil:
+            if xl != get_avltree_nilnode[K](): xl.p = xp
+            if xp != get_avltree_nilnode[K]():
                 if xp.l == x: xp.l = xl
                 else: xp.r = xl
-            if xp.isNil: result = xl
+            if xp == get_avltree_nilnode[K](): result = xl
             else: result = xp.rebalance_to_root
         else:
             var nxtp = nxt.p
             var nxtr = nxt.r
-            if not xp.isNil:
+            if xp != get_avltree_nilnode[K]():
                 if xp.l == x: xp.l = nxt
                 else: xp.r = nxt
             nxt.p = xp
             nxt.l = x.l
-            if not nxt.l.isNil: nxt.l.p = nxt
+            if nxt.l != get_avltree_nilnode[K](): nxt.l.p = nxt
             if x.r == nxt:
                 nxt.update
                 result = nxt.rebalance_to_root
             else:
                 if nxtp.l == nxt: nxtp.l = nxtr
                 else: nxtp.r = nxtr
-                if not nxtr.isNil: nxtr.p = nxtp
+                if nxtr != get_avltree_nilnode[K](): nxtr.p = nxtp
                 nxt.r = x.r
                 nxt.r.p = nxt
                 nxt.update
                 result = nxtp.rebalance_to_root
-        x.l = nil
-        x.r = nil
-        x.p = nil
+        x.l = get_avltree_nilnode[K]()
+        x.r = get_avltree_nilnode[K]()
+        x.p = get_avltree_nilnode[K]()
         x.update
     proc next*[K](node: AvlTreeNode[K]): AvlTreeNode[K] =
         var node = node
-        if not node.r.isNil:
+        if node.r != get_avltree_nilnode[K]():
             node = node.r
-            while not node.l.isNil: node = node.l
+            while node.l != get_avltree_nilnode[K](): node = node.l
             return node
-        while not node.p.isNil and node.p.r == node: node = node.p
+        while node.p.r == node: node = node.p
         return node.p
     proc prev*[K](node: AvlTreeNode[K]): AvlTreeNode[K] =
         var node = node
-        if not node.l.isNil:
+        if node.l != get_avltree_nilnode[K]():
             node = node.l
-            while not node.r.isNil: node = node.r
+            while node.r != get_avltree_nilnode[K](): node = node.r
             return node
-        while not node.p.isNil and node.p.l == node: node = node.p
+        while node.p.l == node: node = node.p
         return node.p
     proc get*[K](node: AvlTreeNode[K], idx: int): AvlTreeNode[K] =
         assert idx >= 0
-        if idx >= node.len: return nil
+        if idx >= node.len: return get_avltree_nilnode[K]()
         result = node
         var idx = idx
-        while (result.l.isNil and idx != 0) or (not result.l.isNil and result.l.len != idx):
-            if result.l.isNil or result.l.len < idx:
-                idx -= (if result.l.isNil: 1 else: result.l.len + 1)
-                assert(not result.r.isNil)
+        while result.l.len != idx:
+            if result.l.len < idx:
+                idx -= result.l.len + 1
                 result = result.r
             else:
                 result = result.l
     proc index*[K](node: AvlTreeNode[K]): int =
         var node = node
-        if node.isNil: return 0
+        if node == get_avltree_nilnode[K](): return 0
         result = node.l.len
-        while not node.p.isNil:
+        while node.p != get_avltree_nilnode[K]():
             if node.p.r == node: result += node.p.l.len + 1
             node = node.p
