136f9a7cfb209af9817fcd6dbf04d15df9ecc69e
kemuniku <63355030+kemuniku@users.noreply.github.com>
2026-02-11 05:05:44 +0900

    fix cartesian tree

--- src/cplib/tree/cartesiantree.nim
+++ src/cplib/tree/cartesiantree.nim
@@ -1,25 +1,24 @@
 when not declared CPLIB_TREE_CARTESIAN_TREE:
     const CPLIB_TREE_CARTESIAN_TREE* = 1
-    import cplib/graph/graph
     import sequtils
 
     proc cartesian_tree_tuple*(A:seq[int]):seq[tuple[p:int,l:int,r:int]]=
-        ## Cartesian Treeを構築する。 O(N)
-        ## (各頂点の親、左の子、右の子)　を返す。存在しない場合-1が入る。
         result = newseqwith(len(A),(-1,-1,-1))
-        var stack : seq[int] = @[A[0]]
+        var stack : seq[int]
 
-        for i in 1..<(len(A)):
+        for i in 0..<(len(A)):
+            var s = -1
             while len(stack) > 0 and A[stack[^1]] > A[i]:
-                var s = stack.pop()
-                result[i].l = s
+                s = stack.pop()
+            if s != -1:
+                if result[i].l != -1:
+                    swap(result[i].l , result[s].l)
+                else:
+                    result[i].l = s
                 result[s].p = i
             if len(stack) > 0:
                 result[i].p = stack[^1]
                 result[stack[^1]].r = i
             stack.add(i)
 
-
-
-
-
+            #echo stack.mapit(A[it])
