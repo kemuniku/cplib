31a68bea83af7f763f411c8f2330a2c95e337c9b
kemuniku <63355030+kemuniku@users.noreply.github.com>
2026-05-26 10:40:09 +0900

    add kth_element

--- src/cplib/utils/kth_element.nim
+++ src/cplib/utils/kth_element.nim
@@ -1,7 +1,44 @@
 when not declared CPLIB_UTILS_KTH_ELEMENT:
     const COMPETITIVE_UTILS_KTH_ELEMENT* = 1
-    import random
-    proc kth_element*[T](nums : var seq[T], k:int):T=
+    import random,sequtils
+    randomize()
+    proc kth_element*[T](X:seq[T],K:int):T=
+        var now = 0
+        var r = len(X)
+        var C = newseqwith(r,0)
+        var X = X
+        while true:
+            var L = 0
+            var S = 1
+            swap(X[rand(0..<r)],X[r-1])
+            C[r-1] = 0
+            for i in 0..<(r-1):
+                var tmp = cmp(X[i],X[r-1])
+                if tmp < 0:
+                    L += 1
+                elif tmp == 0:
+                    S += 1
+                C[i] = tmp
+            if now+L > K:
+                var idx = 0
+                for i in 0..<(r-1):
+                    if C[i] < 0:
+                        swap(X[i],X[idx])
+                        idx += 1
+                r = idx
+            elif now+L+S > K:
+                return X[r-1]
+            else:
+                var idx = 0
+                for i in 0..<(r-1):
+                    if C[i] > 0:
+                        swap(X[i],X[idx])
+                        idx += 1
+                now += L+S
+                r = idx
+    
+    proc kth_element_break*[T](nums : var seq[T], k:int):T=
+        ## 引数のnumsの順番がバラバラになります！
         var target = k
         var left = 0
         var right = len(nums) - 1
@@ -20,12 +57,13 @@ when not declared CPLIB_UTILS_KTH_ELEMENT:
             var gt = right     # pivotより大きい要素の左端
             
             while i <= gt:
-                if nums[i] < pivot:
+                var c = cmp(nums[i],pivot)
+                if c < 0:
                     swap(nums[lt],nums[i])
                     # nums[lt], nums[i] = nums[i], nums[lt]
                     lt += 1
                     i += 1
-                elif nums[i] > pivot:
+                elif c > 0:
                     swap(nums[i],nums[gt])
                     # nums[i], nums[gt] = nums[gt], nums[i]
                     gt -= 1
