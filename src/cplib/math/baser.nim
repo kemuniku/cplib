3174b22ad24a1373f439e6378e41176585c0b10a
kemuniku <63355030+kemuniku@users.noreply.github.com>
2026-07-05 21:15:28 +0900

    sizeを指定しないときの挙動を追加

--- src/cplib/math/baser.nim
+++ src/cplib/math/baser.nim
@@ -1,9 +1,18 @@
 when not declared(CPLIB_MATH_BASER):
     const CPLIB_MATH_BASER* = 1
 
-    proc baser*(num,base,size:int):seq[int]=
-        var num = num
-        for _ in 0..<size:
-            result.add(num mod base)
-            num = num div base
-        return result
+    proc baser*(num,base:int,size:int = -1):seq[int]=
+        ## K進数に変換する。
+        ## sizeを指定しない場合、その数の桁数となる。0の場合は空配列を返す点に注意。
+        if size == -1:
+            var num = num
+            while num > 0:
+                result.add(num mod base)
+                num = num div base
+            return result
+        else:
+            var num = num
+            for _ in 0..<size:
+                result.add(num mod base)
+                num = num div base
+            return result
