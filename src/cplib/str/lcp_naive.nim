8bf422b5815f2fec536a03ed0985cfcd65d2f971
kemuniku <63355030+kemuniku@users.noreply.github.com>
2026-07-05 23:03:18 +0900

    openArrayを用いる

--- src/cplib/str/lcp_naive.nim
+++ src/cplib/str/lcp_naive.nim
@@ -1,6 +1,6 @@
 when not declared CPLIB_STR_LCP_NAIVE:
     const CPLIB_STR_LCP_NAIVE* = 1
-    proc lcp_naive*(S,T:seq or string):int=
+    proc lcp_naive*[U](S,T:openArray[U]):int=
         for i in 0..<(min(len(S),len(T))):
             if S[i] != T[i]:
                 return i
