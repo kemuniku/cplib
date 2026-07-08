b7341c4c6ed0592ca45a445255db2baae7b9bbe0
kemuniku <63355030+kemuniku@users.noreply.github.com>
2026-07-09 02:51:42 +0900

    unionfindの中身をint32にする

--- src/cplib/collections/rollback_unionfind.nim
+++ src/cplib/collections/rollback_unionfind.nim
@@ -3,26 +3,29 @@ when not declared CPLIB_COLLECTIONS_ROLLBACK_UNIONFIND:
     import sequtils, strformat
     type RollbackUnionFind* = object
         count: int
-        par_or_siz: seq[int]
-        history: seq[(int, int)]
+        par_or_siz: seq[int32]
+        history: seq[(int, int32)]
         snap: int
     proc count*(self: RollbackUnionFind): int = self.count
     proc get_state*(self: RollbackUnionFind): int = (self.history.len shr 1)
     proc initRollbackUnionFind*(n: int): RollbackUnionFind =
-        RollbackUnionFind(count: n, par_or_siz: newSeqWith(n, -1), history: newSeq[(int, int)](), snap: 0)
-    proc root*(self: RollbackUnionFind, x: int): int = (if self.par_or_siz[x] < 0: x else: self.root(self.par_or_siz[x]))
-    proc issame*(self: RollbackUnionFind, x, y: int): bool = self.root(x) == self.root(y)
+        RollbackUnionFind(count: n, par_or_siz: newSeqWith(n, -1'i32), history: newSeq[(int, int32)](), snap: 0)
+    proc root_i32(self: RollbackUnionFind, x: int): int32 = (if self.par_or_siz[x] < 0: x.int32 else: self.root_i32(self.par_or_siz[x].int))
+    proc root*(self: RollbackUnionFind, x: int): int = self.root_i32(x).int
+    proc issame*(self: RollbackUnionFind, x, y: int): bool = self.root_i32(x) == self.root_i32(y)
     proc unite*(self: var RollbackUnionFind, x, y: int): bool {.discardable.} =
-        var x = self.root(x)
-        var y = self.root(y)
-        var sx = self.par_or_siz[x]
-        var sy = self.par_or_siz[y]
-        self.history.add((x, sx))
-        self.history.add((y, sy))
+        var x = self.root_i32(x)
+        var y = self.root_i32(y)
+        var sx = self.par_or_siz[x.int]
+        var sy = self.par_or_siz[y.int]
+        self.history.add((x.int, sx))
+        self.history.add((y.int, sy))
         if x == y: return false
         if sx > sy: swap(x, y)
-        self.par_or_siz[x] += self.par_or_siz[y]
-        self.par_or_siz[y] = x
+        let xi = x.int
+        let yi = y.int
+        self.par_or_siz[xi] += self.par_or_siz[yi]
+        self.par_or_siz[yi] = x
         return true
     proc undo*(self: var RollbackUnionFind) =
         assert self.history.len > 0, "Can't undo because Unionfind is already initial state."
@@ -37,5 +40,5 @@ when not declared CPLIB_COLLECTIONS_ROLLBACK_UNIONFIND:
         assert state <= self.history.len, &"Rollback state must be the same or smaller than current state. state: {state}, self.history.len: {self.history.len}"
         while state < self.history.len: self.undo()
     proc siz*(self: RollbackUnionFind, x: int): int =
-        var x = self.root(x)
-        return -self.par_or_siz[x]
+        var x = self.root_i32(x)
+        return (-self.par_or_siz[x.int]).int
--- src/cplib/collections/rootvalue_unionfind.nim
+++ src/cplib/collections/rootvalue_unionfind.nim
@@ -3,46 +3,50 @@ when not declared CPLIB_COLLECTIONS_ROOT_VALUE_UNIONFIND:
     import algorithm, sequtils
     type RootValueUnionFind*[T] = ref object
         count*: int
-        par_or_siz: seq[int]
+        par_or_siz: seq[int32]
         op : proc(x,y:var T)
         values* : seq[T]
     proc initRootValueUnionFind*[T](N: int,op:(proc(x,y:var T)),default:(proc():T)): RootValueUnionFind[T] =
         ## opについて、xのほうが新しくrootになるものとする(よって、xのサイズはyのサイズ以上である)
         ## 関数にvarで与えられるので、関数内でxを書き換えてください
         ## なんでこうしてるの？　-> HashSetとかを載せてマージできると嬉しさがあるので。
-        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1),op:op,values:newseqwith(N,default()))
+        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1'i32),op:op,values:newseqwith(N,default()))
     proc initRootValueUnionFind*[T](N: int,op:(proc(x,y:var T)),default:T): RootValueUnionFind[T] =
         ## opについて、xのほうが新しくrootになるものとする(よって、xのサイズはyのサイズ以上である)
         ## 関数にvarで与えられるので、関数内でxを書き換えてください
         ## なんでこうしてるの？　-> HashSetとかを載せてマージできると嬉しさがあるので。
-        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1),op:op,values:newseqwith(N,default))
+        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1'i32),op:op,values:newseqwith(N,default))
     proc initRootValueUnionFind*[T](N: int,op:(proc(x,y:var T)),values:openArray[T]): RootValueUnionFind[T] =
         ## opについて、xのほうが新しくrootになるものとする(よって、xのサイズはyのサイズ以上である)
         ## 関数にvarで与えられるので、関数内でxを書き換えてください
         ## なんでこうしてるの？　-> HashSetとかを載せてマージできると嬉しさがあるので。
-        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1),op:op,values: @values)
-    proc root*[T](self: RootValueUnionFind[T], x: int): int =
+        result = RootValueUnionFind[T](count: N, par_or_siz: newSeqwith(N, -1'i32),op:op,values: @values)
+    proc root_i32[T](self: RootValueUnionFind[T], x: int): int32 =
         if self.par_or_siz[x] < 0:
-            return x
+            return x.int32
         else:
-            self.par_or_siz[x] = self.root(self.par_or_siz[x])
+            self.par_or_siz[x] = self.root_i32(self.par_or_siz[x].int)
             return self.par_or_siz[x]
+    proc root*[T](self: RootValueUnionFind[T], x: int): int =
+        return self.root_i32(x).int
     proc issame*[T](self: RootValueUnionFind[T], x: int, y: int): bool =
-        return self.root(x) == self.root(y)
+        return self.root_i32(x) == self.root_i32(y)
     proc unite*[T](self: RootValueUnionFind[T], x: int, y: int) =
-        var x = self.root(x)
-        var y = self.root(y)
+        var x = self.root_i32(x)
+        var y = self.root_i32(y)
         if(x != y):
-            if(self.par_or_siz[x] > self.par_or_siz[y]):
+            if(self.par_or_siz[x.int] > self.par_or_siz[y.int]):
                 swap(x, y)
-            self.op(self.values[x],self.values[y])
-            self.par_or_siz[x] += self.par_or_siz[y]
-            self.par_or_siz[y] = x
+            let xi = x.int
+            let yi = y.int
+            self.op(self.values[xi],self.values[yi])
+            self.par_or_siz[xi] += self.par_or_siz[yi]
+            self.par_or_siz[yi] = x
             self.count -= 1
     proc siz*[T](self: RootValueUnionFind[T], x: int): int =
-        var x = self.root(x)
-        return -self.par_or_siz[x]
+        var x = self.root_i32(x)
+        return (-self.par_or_siz[x.int]).int
     proc get*[T](self: RootValueUnionFind[T],x:int): var T=
-        return self.values[self.root(x)]
+        return self.values[self.root_i32(x).int]
     proc set*[T](self: RootValueUnionFind[T],x:int,value:T)=
-        self.values[self.root(x)] = value
+        self.values[self.root_i32(x).int] = value
