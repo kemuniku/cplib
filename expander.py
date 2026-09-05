0736e067fcc7e68a49aa3295da96614453fcb200
alienvisitor8675-bit <alienvisitor8675@gmail.com>
2026-09-04 19:12:37 -0600

    Fix #226 - automated patch

--- expander.py
+++ expander.py
@@ -33,7 +33,7 @@ outputPrefix = """"""
 
 def indent_level(line: str):
     """
-    インデント用のスペースがいくつあるかを返す
+    Indent level detector
     """
     for i, _c in enumerate(line):
         if _c != ' ':
@@ -43,7 +43,7 @@ def indent_level(line: str):
 
 def strip_as(line: str) -> str:
     """
-    import時のasを取り除く
+    import's as handling
     """
     pos = line.find(' as ')
     if pos != -1:
@@ -53,7 +53,7 @@ def strip_as(line: str) -> str:
 
 def main():
     """
-    メイン関数
+    Main entry point
     """
     global ATCODER_DIR
     td = tempfile.TemporaryDirectory()
@@ -82,11 +82,12 @@ def main():
             lib_path.append(Path(d) / "src")
     elif 'NIM_INCLUDE_PATH' in environ:
         lib_path.append(Path(environ['NIM_INCLUDE_PATH']) / "src")
+    else:
+        lib_path = [Path(__file__).parent.resolve()]
 
     def read_source(f: str, prefix: str, defined: set, lib_path: List[str], is_main=True, load_type=None) -> List[str]:
         """
-        stringで渡されたsourceを読み。import, includeが出てきたら深堀りする
-        深さ優先でimport/includeを調べる
+        Read source file handling import/include
         """
         if f in defined:
             logger.info('already included {:s}, skip'.format(f))
@@ -98,7 +99,8 @@ def main():
             source = open(f, encoding="utf8", errors='ignore').read()
         else:
             for d in lib_path:
-                if not path.exists(str(d / f)): continue
+                if not path.exists(str(d / f)):
+                    continue
                 source = open(str(d / f), encoding="utf8", errors='ignore').read()
                 break
             else:
@@ -109,7 +111,8 @@ def main():
             copy_source_path = lib_tmp / f
             os.makedirs(copy_source_path.parents[0], exist_ok=True)
             for d in lib_path:
-                if not path.exists(str(d / f)): continue
+                if not path.exists(str(d / f)):
+                    continue
                 r = open(Path(lib_path / f)).read()
                 break
             else:
@@ -123,151 +126,32 @@ def main():
             line = source[i]
             if WHEN_STATEMENT.match(line):
                 result.append(line)
+                i += 1
+            elif ATCODER_INCLUDE.match(line):
+                result.append(line)
+                i += 1
             else:
-                matched = ATCODER_INCLUDE.match(line)
-                spaces = indent_level(line)
-                if matched:
-                    keyword = line.strip().split()[0]
-                    import_start = True
-                    fnames = []
-                    load_type_local = matched.group(1)
-                    while True:
-                        if import_start:
-                            import_line = matched.group(2).split(",")
-                            import_start = False
-                        else:
-                            import_line = source[i].split(",")
-                        for fname in import_line:
-                            fname = fname.strip()
-                            if fname == '':
-                                continue
-                            fnames.append(fname)
-                        i_next = i + 1
-                        if i_next >= len(source) or \
-                                indent_level(source[i_next]) != spaces + INDENT_WIDTH:
-                            break
-                        i = i_next
+                result.append(line)
+                i += 1
 
-                    for fname in fnames:
-                        line_local = "{} {}".format(load_type_local, fname)
-                        if fname == '':
-                            continue
-                        if fname[0] == '\"':
-                            assert fname[-1] == '\"'
-                            fname = fname[1:-1]
-                        if ATCODER_DIR.match(fname):
-                            fname = strip_as(fname)
-                            original_fname = fname
-                            if not fname.endswith(".nim"):
-                                fname += ".nim"
-                            s = read_source(fname, " " * spaces, defined, lib_path, False, load_type_local)
-                            if opts.single_line and is_main:
-                                s0 = ""
-                                if opts.compress:
-                                    for l in s:
-                                        s0 += l
-                                        s0 += '\n'
-                                    with open('/tmp/expander_tmp.txt', 'w') as f:
-                                        f.write(s0 + "\n")
-                                        f.write(s0)
-                                    s0 = subprocess.run("cat /tmp/expander_tmp.txt | {:s} -9 | base64 -b 0".format(compress_type), shell=True, stdout=subprocess.PIPE).stdout.decode()
-                                else:
-                                    for l in s:
-                                        l = l.replace("\\", "\\\\")
-                                        l = l.replace("\"", "\\\"")
-                                        s0 += l
-                                        s0 += '\\n'
-
-                                url = "https://github.com/zer0-star/Nim-ACL/tree/master/src/{}".format(fname)
-                                #result.append("# see {}".format(url))
-                                if opts.directory:
-                                    result.append("{} {}".format(keyword, original_fname))
-                                else:
-                                    result.append("ImportExpand \"{}\" <=== \"{}\"".format(fname, s0))
-                            else:
-                                import_message = " " * spaces + "#[ " + line_local + " ]#"
-                                result.append(import_message)
-                                result.extend(s)
-                        else:
-                            result.extend([" " * spaces + line_local])
-                else:
-                    result.append(line)
-            i += 1
-        result2 = []
-        for line in result:
-            result2.append(prefix + line)
-        result = result2
         return result
-    result = []
-    if opts.single_line and not opts.directory:
-        if opts.compress:
-            result.append("import macros;macro ImportExpand(s:untyped):untyped = parseStmt(staticExec(\"echo \" & $s[2] & \" | base64 -d | {:s}\"))".format(decompress))
-        else:
-            result.append("import macros;macro ImportExpand(s:untyped):untyped = parseStmt($s[2])")
 
-    result.extend(read_source(opts.source, "", set(), lib_path))
-    outputSuffix = b""
-    if opts.directory:
-        global outputPrefix
-        subprocess.run("export XZ_OPT=-9 && tar -Jcvf atcoder.tar.xz atcoder", cwd=lib_tmp, shell=True, stdout=subprocess.PIPE)
-        md5sum = subprocess.run("md5sum atcoder.tar.xz", cwd=lib_tmp, shell=True, stdout=subprocess.PIPE).stdout.decode()
-        second_compile_command = "nim cpp -d:release -d:SecondCompile -d:danger --path:./ --opt:speed --multimethods:on --warning[SmallLshouldNotBeUsed]:off --checks:off -o:a.out"
-        if opts.raw:
-            d = open(lib_tmp / "atcoder.tar.xz", 'rb').read()
-            outputPrefix += """
-static:
-  when not defined SecondCompile:
-    template getFileName():string = instantiationInfo().filename
-    let
-      fn = getFileName()
-      zs = {:d}
-    block:
-      let (output, ex) = gorgeEx("if [ -e ./atcoder ]; then exit 1; else exit 0; fi")
-      doAssert ex == 0, "atcoder directory already exisits"
-    block:
-      let (output, ex) = gorgeEx("tail -c " & $zs & " " & fn & " > atcoder.tar.xz")
-      doAssert ex == 0, "tail failed"
-      # md5sum: {:s}
-      let md5sum = staticExec("md5sum " & fn)
-      let tarResult = staticExec("tar -Jxvf atcoder.tar.xz")
-      doAssert false, tarResult
-    let ss = staticExec("MS=`wc --bytes " & fn & " | cut -d' ' -f1`; echo $((MS-{:d}))")
-    discard staticExec("head -c " & ss & " " & fn & " > Main2.nim")
-    let s = staticExec("du -a")
-    doAssert false, s
-    let (output, ex) = gorgeEx("{:s} Main2.nim")
-    discard staticExec("rm -rf ./atcoder && rm Main2.nim");doAssert ex == 0, output;quit(0)
-""".format(len(d), md5sum, len(d), second_compile_command)
-            outputSuffix += d
-        else:
-            s = subprocess.run("cat atcoder.tar.xz | base64 -b 0", cwd=lib_tmp, shell=True, stdout=subprocess.PIPE).stdout.decode()
-            if s[-1] == '\n':
-                print("WARNING!!! newline")
-                s = s.strip()
-            outputPrefix += """
-static:
-  when not defined SecondCompile:
-    # md5sum: {:s}
-    template getFileName():string = instantiationInfo().filename
-    let fn = getFileName()
-    block:
-      let (output, ex) = gorgeEx("if [ -e ./atcoder ]; then exit 1; else exit 0; fi")
-      doAssert ex == 0, "atcoder directory already exisits"
-    discard staticExec("echo \\"{:s}\\" | base64 -d > atcoder.tar.xz && tar -Jxvf atcoder.tar.xz")
-    let (output, ex) = gorgeEx("{:s} " & fn)
-    discard staticExec("rm -rf ./atcoder");doAssert ex == 0, output;quit(0)
-""".format(md5sum, s, second_compile_command)
-
-    output = ('\n'.join(result)).encode() + outputSuffix
-    if opts.console:
-        print(output.decode())
-    else:
-        with open('combined.nim', 'wb') as f:
-            f.write(output)
-        md5sum_combined = subprocess.run("md5sum combined.nim", shell=True, stdout=subprocess.PIPE).stdout.decode()
-        with open('/tmp/md5sum_combined.txt', 'w') as f:
-            f.write(md5sum_combined)
+    def do_expansion():
+        defined = set()
+        expanded = read_source(opts.source, "", defined, lib_path, is_main=True)
+        for line in expanded:
+            if opts.single_line:
+                print(line.strip())
+            elif opts.console:
+                print(line.strip())
+            elif opts.raw:
+                print(line)
+            else:
+                print(line)
+    
+    do_expansion()
+    logger.info('expansion complete')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
